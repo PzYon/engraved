c037e93e82969db48875962b815ba1b092d549e7
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-01-18 00:52:53 +0100

    Hello World API

---
+++ api/Metrix.Api/Metrix.Api/Controllers/SaliMaxController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Metrix.Api.Controllers
+{
+    public class SaliMax
+    {
+        public DateTime Date { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SaliMaxController : ControllerBase
+    {
+        [HttpGet(Name = "Sali Max!")]
+        public SaliMax Get()
+        {
+            return new SaliMax { Date = DateTime.Now };
+        }
+    }
+}
