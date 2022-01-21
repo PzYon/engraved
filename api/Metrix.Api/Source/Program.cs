6c7e3c7b7a816451bf66c225b6d8f4ed442bd8f4
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-01-21 23:37:32 +0100

    UseCors

--- api/Metrix.Api/Source/Program.cs
+++ api/Metrix.Api/Source/Program.cs
@@ -2,14 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
-{
-  options.AddPolicy(
-    "All",
-    policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()
-  );
-});
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +16,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
