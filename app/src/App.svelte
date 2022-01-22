43b85f32df86d1682e5004a171e9ef76a80e8393
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-01-22 23:34:02 +0100

    Add Svelte app

---
+++ app/src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let resultMessage = "";
+	let errorMessage = "";
+
+	fetch(new Request("https://metrix-api.azurewebsites.net/salimax"))
+			.then(response => response.json())
+			.then(result =>  resultMessage = "Success: " + JSON.stringify(result))
+			.catch(error => errorMessage = "Error: " + JSON.stringify(error));
+</script>
+
+<main>
+	<h1>Sali Max</h1>
+	<div class="success">{resultMessage}</div>
+	<div class="error">{errorMessage}</div>
+</main>
+
+<style>
+	.success {
+		color: darkgreen;
+	}
+
+	.error {
+		color: darkred;
+	}
+</style>
