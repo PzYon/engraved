2310032b7389543f94b8e6b6ce86ad81cd324944
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-01-23 21:54:09 +0100

    Call measurements endpoint from Svelte component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
-	let resultMessage = "";
-	let errorMessage = "";
+  import {constants} from "./constants";
+  import Measurements from "./Measurements.svelte";
 
-	fetch(new Request("https://metrix-api.azurewebsites.net/api/salimax"))
-			.then(response => response.json())
-			.then(result =>  resultMessage = "From server: " + JSON.stringify(result))
-			.catch(error => errorMessage = "Server error: " + JSON.stringify(error));
+  let resultMessage = "";
+  let errorMessage = "";
+
+  fetch(new Request(constants.apiBaseUrl + "/salimax"))
+    .then(response => response.json())
+    .then(result => resultMessage = "From server: " + JSON.stringify(result))
+    .catch(error => errorMessage = "Server error: " + JSON.stringify(error));
 </script>
 
 <main>
-	<h1>Sali Max</h1>
-	<div class="success">{resultMessage}</div>
-	<div class="error">{errorMessage}</div>
+    <h1>Sali Max</h1>
+    <div class="success">{resultMessage}</div>
+    <div class="error">{errorMessage}</div>
 </main>
 
-<style>
-	.success {
-		color: darkgreen;
-		font-weight: bold;
-	}
+<Measurements/>
 
-	.error {
-		color: darkred;
-	}
+<style>
+    .success {
+        color: darkgreen;
+        font-weight: bold;
+    }
+
+    .error {
+        color: darkred;
+    }
 </style>
