06c9d3a5cef757b5e29f6a686420dde59fd04513
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-01-23 14:32:07 +0100

    WIP

---
+++ api/Metrix.Core/Source/Domain/Metric.cs
@@ -0,0 +1,35 @@
+﻿namespace Metrix.Core.Domain;
+
+public enum MetricType
+{
+  Counter,
+  Gauge,
+  Timed
+}
+
+public enum MetricUnit
+{
+}
+
+public class Metric
+{
+  public string Name { get; set; }
+
+  public string Description { get; set; }
+
+  public MetricType Type { get; set; }
+
+  public MetricUnit Unit { get; set; }
+}
+
+
+public class Measurement
+{
+  public Metric Metric { get; set; }
+
+  public long Value { get; set; }
+
+  public DateTime DateTime { get; set; }
+
+  public string Notes { get; set; }
+}
