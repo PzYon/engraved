9535d6f230438cb6e6f3d0ff40e1441b8889d229
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-02-05 21:58:35 +0100

    Lazy load chartjs
    Issue #14

--- app/src/components/charts/Visualization.tsx
+++ app/src/components/charts/Visualization.tsx
@@ -1,39 +1,14 @@
-import { IMeasurement } from "../../serverApi/IMeasurement";
-import React from "react";
-import {
-  BarController,
-  BarElement,
-  CategoryScale,
-  Chart as ChartJS,
-  Legend,
-  LinearScale,
-  LineController,
-  LineElement,
-  PointElement,
-  TimeScale,
-  Title,
-  Tooltip,
-} from "chart.js";
-import { Chart } from "react-chartjs-2";
-import "chartjs-adapter-date-fns";
-import { createChart } from "./createChart";
+import React, { Suspense } from "react";
+import { IVisualizationProps } from "./IVisualizationProps";
 
-ChartJS.register(
-  BarController,
-  BarElement,
-  CategoryScale,
-  Legend,
-  LinearScale,
-  LineController,
-  LineElement,
-  PointElement,
-  TimeScale,
-  Title,
-  Tooltip
-);
+export const Visualization: React.FC<IVisualizationProps> = (
+  props: IVisualizationProps
+) => {
+  const ChartJs = React.lazy(() => import("./LazyChartJs"));
 
-export const Visualization: React.FC<{ measurements: IMeasurement[] }> = ({
-  measurements,
-}) => {
-  return <Chart {...createChart("bar", measurements)} />;
+  return (
+    <Suspense fallback={<div />}>
+      <ChartJs {...props} />
+    </Suspense>
+  );
 };
