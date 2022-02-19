4d6e85299eb07b93bbef8b4194ae621d25406876
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-02-19 21:42:06 +0100

    Show notes in data grid

--- api/Metrix.Core/Source/Domain/DummyMetricsLoader.cs
+++ api/Metrix.Core/Source/Domain/DummyMetricsLoader.cs
@@ -1,16 +1,13 @@
-﻿using System.Text;
-using static System.String;
-
-namespace Metrix.Core.Domain;
+﻿namespace Metrix.Core.Domain;
 
 public class DummyMetricsLoader : IMetricsLoader
 {
   private static readonly Metric[] _metrics = Enumerable.Range(0, Random.Shared.Next(5, 30))
     .Select(i => new Metric
     {
-      Description = LoremIpsum(0, 12, 1, 3),
+      Description = LoremUtil.LoremIpsum(0, 12, 1, 3),
       Key = "key" + i,
-      Name = LoremIpsum(1, 3, 1, 1),
+      Name = LoremUtil.LoremIpsum(1, 3, 1, 1),
       Type = MetricType.Gauge,
       Unit = "kg"
     })
@@ -26,42 +23,4 @@ public class DummyMetricsLoader : IMetricsLoader
     return _metrics.First(m => m.Key == metricKey);
   }
 
-  // https://stackoverflow.com/questions/4286487/is-there-any-lorem-ipsum-generator-in-c
-  private static string LoremIpsum(int minWords, int maxWords, int minSentences, int maxSentences)
-  {
-    var words = new[]
-    {
-      "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
-      "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
-      "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
-    };
-
-    var rand = new Random();
-    int numSentences = rand.Next(minSentences, maxSentences);
-    int numWords = rand.Next(minWords, maxWords);
-
-    if (numWords == 0 || numSentences == 0)
-    {
-      return Empty;
-    }
-
-    StringBuilder result = new StringBuilder();
-
-    for (int s = 0; s < numSentences; s++)
-    {
-      for (int w = 0; w < numWords; w++)
-      {
-        if (w > 0)
-        {
-          result.Append(" ");
-        }
-
-        result.Append(words[rand.Next(words.Length)]);
-      }
-
-      result.Append(". ");
-    }
-
-    return result.ToString();
-  }
 }
--- app/src/i18n/translations.ts
+++ app/src/i18n/translations.ts
@@ -1,6 +1,9 @@
 import { ITranslations } from "./ITranslations";
 
 export const translations: ITranslations = {
+  columnName_date: "Date",
+  columnName_notes: "Notes",
+  columnName_value: "Value",
   create: "Create",
   label_metricName: "Name",
   tab_measurements: "Measurements",
