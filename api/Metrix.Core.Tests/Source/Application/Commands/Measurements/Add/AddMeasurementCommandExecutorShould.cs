e0c9f44d7830e333f4ac1a1e21617eed8d498291
Markus Doggweiler <PzYon@users.noreply.github.com>
2022-03-10 00:09:53 +0100

    AddMeasurementCommand (incl. Tests) (#31)

---
+++ api/Metrix.Core.Tests/Source/Application/Commands/Measurements/Add/AddMeasurementCommandExecutorShould.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Metrix.Core.Application.Persistence;
+using Metrix.Core.Domain.Measurements;
+using Metrix.Core.Domain.Metrics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Metrix.Core.Application.Commands.Measurements.Add;
+
+[TestClass]
+public class AddMeasurementCommandExecutorShould
+{
+  private TestDb testDb;
+  private AddMeasurementCommandExecutor executor;
+
+  [TestInitialize]
+  public void SetUp()
+  {
+    testDb = new TestDb();
+    executor = new AddMeasurementCommandExecutor();
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(InvalidCommandException))]
+  public void Throw_WhenMetricKeyIsNotSpecified()
+  {
+    var command = new AddMeasurementCommand { MetricKey = string.Empty };
+
+    executor.Execute(testDb, command);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(InvalidCommandException))]
+  public void Throw_WhenMetricDoesNotExist()
+  {
+    var command = new AddMeasurementCommand
+    {
+      MetricKey = "k3y",
+      Notes = "n0t3s",
+      Value = 42
+    };
+
+    executor.Execute(testDb, command);
+  }
+
+  [TestMethod]
+  public void Set_Value1_WhenTypeIsCounter()
+  {
+    testDb.Metrics.Add(new Metric
+    {
+      Key = "k3y",
+      Type = MetricType.Counter
+    });
+
+    var command = new AddMeasurementCommand
+    {
+      MetricKey = "k3y",
+      Notes = "n0t3s"
+    };
+
+    executor.Execute(testDb, command);
+
+    Assert.AreEqual(1, testDb.Measurements.Count);
+
+    Measurement createdMeasurement = testDb.Measurements.First();
+    Assert.AreEqual(command.MetricKey, createdMeasurement.MetricKey);
+    Assert.AreEqual(command.Notes, createdMeasurement.Notes);
+    Assert.AreEqual(1, createdMeasurement.Value);
+  }
+  
+  [TestMethod]
+  public void Set_ValueFromCommand_WhenTypeIsGauge()
+  {
+    testDb.Metrics.Add(new Metric
+    {
+      Key = "k3y",
+      Type = MetricType.Gauge
+    });
+
+    var command = new AddMeasurementCommand
+    {
+      MetricKey = "k3y",
+      Notes = "n0t3s",
+      Value = 123.45
+    };
+
+    executor.Execute(testDb, command);
+
+    Assert.AreEqual(1, testDb.Measurements.Count);
+
+    Measurement createdMeasurement = testDb.Measurements.First();
+    Assert.AreEqual(command.MetricKey, createdMeasurement.MetricKey);
+    Assert.AreEqual(command.Notes, createdMeasurement.Notes);
+    Assert.AreEqual(123.45, createdMeasurement.Value);
+  }
+  
+  [TestMethod]
+  [ExpectedException(typeof(InvalidCommandException))]
+  public void Throw_WhenTypeIsGaugeAndValueIsNull()
+  {
+    testDb.Metrics.Add(new Metric
+    {
+      Key = "k3y",
+      Type = MetricType.Gauge
+    });
+
+    var command = new AddMeasurementCommand
+    {
+      MetricKey = "k3y",
+      Value = null
+    };
+
+    executor.Execute(testDb, command);
+  }
+
+  private class TestDb : IDb
+  {
+    public List<Measurement> Measurements { get; } = new();
+
+    public List<Metric> Metrics { get; } = new();
+  }
+}
--- api/Metrix.Core/Source/Application/Commands/Measurements/Add/AddMeasurementCommandExecutor.cs
+++ api/Metrix.Core/Source/Application/Commands/Measurements/Add/AddMeasurementCommandExecutor.cs
@@ -1,5 +1,6 @@
 using Metrix.Core.Application.Persistence;
 using Metrix.Core.Domain.Measurements;
+using Metrix.Core.Domain.Metrics;
 
 namespace Metrix.Core.Application.Commands.Measurements.Add;
 
@@ -7,12 +8,39 @@ public class AddMeasurementCommandExecutor : ICommandExecutor<AddMeasurementComm
 {
   public void Execute(IDb db, AddMeasurementCommand command)
   {
+    if (string.IsNullOrEmpty(command.MetricKey))
+    {
+      throw CreateInvalidCommandException($"A {nameof(AddMeasurementCommand.MetricKey)} must be specified.");
+    }
+
+    Metric? metric = db.Metrics.FirstOrDefault(m => m.Key == command.MetricKey);
+    if (metric == null)
+    {
+      throw CreateInvalidCommandException($"A metric with key \"{command.MetricKey}\" does not exist.");
+    }
+
     db.Measurements.Add(new Measurement
     {
       MetricKey = command.MetricKey,
       Notes = command.Notes,
       DateTime = DateTime.UtcNow,
-      Value = 1
+      Value = GetValue(command, metric.Type)
     });
+  }
+
+  private static double GetValue(AddMeasurementCommand command, MetricType metricType)
+  {
+    double? value = metricType == MetricType.Counter ? 1 : command.Value;
+    if (value.HasValue)
+    {
+      return value.Value;
+    }
+
+    throw CreateInvalidCommandException($"A \"{nameof(AddMeasurementCommand.Value)}\" must be specified.");
+  }
+
+  private static InvalidCommandException CreateInvalidCommandException(String message)
+  {
+    return new InvalidCommandException(typeof(AddMeasurementCommand), message);
   }
 }
