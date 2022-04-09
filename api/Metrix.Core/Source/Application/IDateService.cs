6887ceb0f0df9e22db3bd9520d3265cc8c983538
Markus Doggweiler <PzYon@users.noreply.github.com>
2022-04-10 01:26:21 +0200

    Use commands to populate MockDb (#57)

--- api/Metrix.Core/Source/Application/IDateService.cs
+++ api/Metrix.Core/Source/Application/IDateService.cs
@@ -9,3 +9,45 @@ public class DateService : IDateService
 {
   public DateTime UtcNow { get; } = DateTime.UtcNow;
 }
+
+public class FakeDateService : IDateService
+{
+  public DateTime UtcNow { get; set; }
+
+  public FakeDateService() : this(DateTime.UtcNow) { }
+
+  public FakeDateService(DateTime initialDate)
+  {
+    UtcNow = initialDate;
+  }
+
+  public void SetNext(int remainingSteps)
+  {
+    TimeSpan diffToNow = DateTime.UtcNow - UtcNow;
+    double hoursPerStep = diffToNow.TotalHours / remainingSteps;
+    var minutesPerStep = Convert.ToInt32(hoursPerStep*60);
+    UtcNow = UtcNow.AddMinutes(Random.Shared.Next(1, minutesPerStep));
+  }
+}
+
+public class SelfIncrementingDateService : IDateService
+{
+  private DateTime _utcNow;
+  private readonly int _initialDayOffset;
+
+  public DateTime UtcNow
+  {
+    get
+    {
+      DateTime next = _utcNow;
+      _utcNow = DateTime.UtcNow.AddHours(-Random.Shared.Next(0, _initialDayOffset * 24));
+      return next;
+    }
+  }
+
+  public SelfIncrementingDateService()
+  {
+    _initialDayOffset = Random.Shared.Next(100, 600);
+    _utcNow = DateTime.UtcNow.AddDays(-_initialDayOffset);
+  }
+}
--- api/Metrix.Core/Source/Application/Persistence/MockDb.cs
+++ api/Metrix.Core/Source/Application/Persistence/MockDb.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text;
+using Metrix.Core.Application.Commands.Measurements.Add;
+using Metrix.Core.Application.Commands.Metrics.Add;
 using Metrix.Core.Domain.Measurements;
 using Metrix.Core.Domain.Metrics;
 
@@ -6,74 +8,106 @@ namespace Metrix.Core.Application.Persistence;
 
 public class MockDb : IDb
 {
+  public List<IMeasurement> Measurements { get; } = new();
+
+  public List<IMetric> Metrics { get; } = new();
+
   public MockDb()
   {
-    Metrics.AddRange(
-      Enumerable.Range(0, Random.Shared.Next(5, 30))
-        .Select(
-          i => new GaugeMetric
-          {
-            Description = LoremIpsum(0, 12, 1, 3),
-            Key = "key" + i,
-            Name = LoremIpsum(1, 3, 1, 1)
-          }
-        )
-        .ToList()
-    );
-
-    foreach (IMetric metric in Metrics)
-    {
-      Measurements.AddRange(
-        Enumerable.Range(0, Random.Shared.Next(5, 20))
-          .Select(
-            _ => new GaugeMeasurement
-            {
-              DateTime = GetRandomDate(),
-              MetricKey = metric.Key,
-              Value = GetValue(),
-              Notes = Random.Shared.Next(1, 3) > 1 ? LoremIpsum(3, 15, 1, 4) : null!
-            }
-          )
-          .OrderBy(m => m.DateTime)
-          .ToList()
-      );
-    }
-
-    AddTimerMetricIncludingMeasurement();
+    CreateRandomMetricsAndMeasurements();
 
     AddSpecificCases();
   }
 
-  public List<IMeasurement> Measurements { get; } = new();
+  private void CreateRandomMetricsAndMeasurements()
+  {
+    foreach (int metricIndex in Enumerable.Range(0, Random.Shared.Next(5, 30)))
+    {
+      string metricKey = "key" + metricIndex;
+      var command = new AddMetricCommand
+      {
+        Key = metricKey,
+        Description = LoremIpsum(0, 12, 1, 3),
+        Name = LoremIpsum(1, 3, 1, 1),
+        Type = GetRandomMetricType()
+      };
 
-  public List<IMetric> Metrics { get; } = new();
+      var dateService = new SelfIncrementingDateService();
+
+      command.CreateExecutor().Execute(this, dateService);
+
+      IMetric metric = Metrics.First(m => m.Key == metricKey);
+
+      switch (metric)
+      {
+        case CounterMetric counterMetric:
+          AddMeasurements(counterMetric, dateService);
+          break;
+        case GaugeMetric gaugeMetric:
+          AddMeasurements(gaugeMetric, dateService);
+          break;
+        case TimerMetric timerMetric:
+          AddMeasurements(timerMetric, dateService.UtcNow);
+          break;
+        default:
+          throw new Exception($"Metric type \"{metric.Type}\" is not yet supported.");
+      }
+    }
+  }
+
+  private void AddMeasurements(CounterMetric metric, IDateService dateService)
+  {
+    foreach (int measurementIndex in Enumerable.Range(0, Random.Shared.Next(0, 30)))
+    {
+      var command = new AddCounterMeasurementCommand
+      {
+        MetricKey = metric.Key
+      };
+
+      new AddCounterMeasurementCommandExecutor(command).Execute(this, dateService);
+    }
+  }
+
+  private void AddMeasurements(GaugeMetric metric, IDateService dateService)
+  {
+    foreach (int measurementIndex in Enumerable.Range(0, Random.Shared.Next(0, 30)))
+    {
+      var command = new AddGaugeMeasurementCommand
+      {
+        MetricKey = metric.Key,
+        Value = Random.Shared.Next(0, Random.Shared.Next(5, 150))
+      };
+
+      new AddGaugeMeasurementCommandExecutor(command).Execute(this, dateService);
+    }
+  }
+
+  private void AddMeasurements(TimerMetric metric, DateTime metricDate)
+  {
+    FakeDateService dateService = new FakeDateService(metricDate);
+
+    int[] count = Enumerable.Range(0, Random.Shared.Next(0, 30)).ToArray();
+
+    foreach (int measurementIndex in count)
+    {
+      int remainingSteps = (count.Length - measurementIndex) * 2;
+      
+      dateService.SetNext(remainingSteps);
+
+      var startTimerCommand = new StartTimerMeasurementCommand { MetricKey = metric.Key };
+      new StartTimerMeasurementCommandExecutor(startTimerCommand).Execute(this, dateService);
+
+      dateService.SetNext(remainingSteps);
+
+      var endTimerCommand = new EndTimerMeasurementCommand { MetricKey = metric.Key };
+      new EndTimerMeasurementCommandExecutor(endTimerCommand).Execute(this, dateService);
+    }
+  }
 
   private void AddSpecificCases()
   {
     AddSpecificCase(SpecificCases.GetMigraineMedicineCase());
     AddSpecificCase(SpecificCases.GetOffByOneEdgeCase());
-  }
-
-  private void AddTimerMetricIncludingMeasurement()
-  {
-    DateTime date = DateTime.Now.AddMinutes(200);
-    Metrics.Add(
-      new TimerMetric
-      {
-        Key = "my_timer",
-        Name = "My Timer",
-        StartDate = date,
-      }
-    );
-
-    Measurements.Add(
-      new TimerMeasurement
-      {
-        MetricKey = "my_timer",
-        DateTime = date,
-        StartDate = date
-      }
-    );
   }
 
   private void AddSpecificCase(SpecificCase specificCase)
@@ -91,14 +125,9 @@ public class MockDb : IDb
     );
   }
 
-  private static DateTime GetRandomDate()
+  private static MetricType GetRandomMetricType()
   {
-    return DateTime.UtcNow.AddSeconds(-Random.Shared.Next(1000, 100000));
-  }
-
-  private static int GetValue()
-  {
-    return Random.Shared.Next(10, 40);
+    return (MetricType)Random.Shared.Next(0, Enum.GetNames(typeof(MetricType)).Length);
   }
 
   // https://stackoverflow.com/questions/4286487/is-there-any-lorem-ipsum-generator-in-c
