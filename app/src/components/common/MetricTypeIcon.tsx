3cfda8057a1b002d4538a274c39a0b857cdfdb64
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-04-12 23:28:41 +0200

    Cleanup

--- app/src/components/common/MetricTypeIcon.tsx
+++ app/src/components/common/MetricTypeIcon.tsx
@@ -37,8 +37,9 @@ const PageTitleHost = styled("div")`
     color: ${(p) => p.theme.palette.text.primary};
     border-color: ${(p) => p.theme.palette.text.primary};
     border: 2px solid ${(p) => p.theme.palette.text.primary};
-    margin-top: 6px;
+    margin-top: 5px;
     margin-right: 10px;
+    padding: 2px;
   }
 `;
 
