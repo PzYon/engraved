919fb45ce2b26fe39f304eeb3172a0c6065fe705
Markus Doggweiler <PzYon@users.noreply.github.com>
2022-04-12 23:41:27 +0200

    Type specific UI (#59)
    Issue #24

---
+++ app/src/components/common/MetricTypeIcon.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { MetricType } from "../../serverApi/MetricType";
+import { BarChartSharp, PlusOneSharp, TimerSharp } from "@mui/icons-material";
+import { styled } from "@mui/material";
+
+export enum MetricTypeIconStyle {
+  Overview,
+  PageTitle,
+}
+
+export const MetricTypeIcon: React.FC<{
+  type: MetricType;
+  style: MetricTypeIconStyle;
+}> = ({ type, style }) => {
+  const Host =
+    style === MetricTypeIconStyle.Overview ? OverviewHost : PageTitleHost;
+
+  return <Host>{getIcon(type)}</Host>;
+};
+
+function getIcon(type: MetricType) {
+  switch (type) {
+    case MetricType.Counter:
+      return <PlusOneSharp />;
+    case MetricType.Gauge:
+      return <BarChartSharp />;
+    case MetricType.Timer:
+      return <TimerSharp />;
+  }
+}
+
+const PageTitleHost = styled("div")`
+  svg {
+    border-radius: 100%;
+    height: 0.8em;
+    width: 0.8em;
+    color: ${(p) => p.theme.palette.text.primary};
+    border-color: ${(p) => p.theme.palette.text.primary};
+    border: 2px solid ${(p) => p.theme.palette.text.primary};
+    margin-top: 5px;
+    margin-right: 10px;
+    padding: 2px;
+  }
+`;
+
+const OverviewHost = styled("div")`
+  svg {
+    border-radius: 100%;
+    border: 2px solid ${(p) => p.theme.palette.primary.main};
+    padding: 8px;
+    color: ${(p) => p.theme.palette.primary.main};
+    background-color: ${(p) => p.theme.palette.background.default};
+  }
+`;
--- app/src/components/overview/MetricListHeaderActions.tsx
+++ app/src/components/overview/MetricListHeaderActions.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 import { IMetric } from "../../serverApi/IMetric";
 import { useDialogContext } from "../layout/dialogs/DialogContext";
 import { HeaderActions, IAction } from "../layout/HeaderActions";
-import { AddOutlined, Edit, VisibilityOutlined } from "@mui/icons-material";
+import { AddOutlined, Edit } from "@mui/icons-material";
 import { renderAddMeasurementDialog } from "../details/add/renderAddMeasurementDialog";
 
 export const MetricListHeaderActions: React.FC<{ metric: IMetric }> = ({
@@ -23,12 +23,6 @@ export const MetricListHeaderActions: React.FC<{ metric: IMetric }> = ({
         label: "Edit",
         icon: <Edit />,
         href: `/metrics/${metric.key}/edit`,
-      },
-      {
-        key: "view",
-        label: "View",
-        icon: <VisibilityOutlined />,
-        href: `/metrics/${metric.key}`,
       },
     ],
     [metric]
