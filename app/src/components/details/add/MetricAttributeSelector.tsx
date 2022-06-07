336061f758dacff887ebc0729aac34f44613951b
Markus Doggweiler <PzYon@users.noreply.github.com>
2022-06-07 17:24:28 +0200

    Support creating attribute values while adding measurement  (#109)

---
+++ app/src/components/details/add/MetricAttributeSelector.tsx
@@ -0,0 +1,103 @@
+import React, { useMemo } from "react";
+import { IMetricAttribute } from "../../../serverApi/IMetricAttribute";
+import { IMetricAttributeValues } from "../../../serverApi/IMetricAttributeValues";
+import {
+  Autocomplete,
+  createFilterOptions,
+  FilterOptionsState,
+  TextField,
+} from "@mui/material";
+
+interface IOption {
+  key: string;
+  addNewKey?: string;
+  label: string;
+}
+
+const filter = createFilterOptions<IOption>({
+  ignoreCase: true,
+  matchFrom: "any",
+});
+
+export const MetricAttributeSelector: React.FC<{
+  attributeKey: string;
+  attribute: IMetricAttribute;
+  selectedAttributeValues: IMetricAttributeValues;
+  onChange: (attributesValues: IMetricAttributeValues) => void;
+}> = ({ attributeKey, attribute, selectedAttributeValues, onChange }) => {
+  const options = useMemo(
+    () =>
+      Object.entries(attribute.values).map((arr) => ({
+        key: arr[0],
+        label: arr[1],
+      })),
+    [attribute.values]
+  );
+
+  return (
+    <Autocomplete
+      options={options}
+      getOptionLabel={(option) => getOptionLabel(option as IOption)}
+      isOptionEqualToValue={(option, value) =>
+        areOptionsEqual(option as IOption, value as IOption)
+      }
+      filterOptions={filterOptions}
+      onChange={async (_, selectedOption) => {
+        const option: IOption = selectedOption as IOption;
+
+        onChange({
+          ...selectedAttributeValues,
+          ...{ [attributeKey]: [option?.addNewKey || option?.key] },
+        });
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={attribute.name}
+          InputProps={params.InputProps}
+        />
+      )}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+    />
+  );
+
+  function getSelectedAttributeValue(): string {
+    const values = selectedAttributeValues?.[attributeKey] || [];
+    return values[0];
+  }
+
+  function areOptionsEqual(a: IOption, b: IOption): boolean {
+    return a.key == b.key;
+  }
+
+  function filterOptions(
+    options: IOption[],
+    params: FilterOptionsState<IOption>
+  ): IOption[] {
+    const { inputValue } = params;
+
+    const isExisting = options.some(
+      (o) => inputValue?.toLowerCase() === o.label?.toLowerCase()
+    );
+
+    const filtered = filter(options, params);
+
+    if (inputValue !== "" && !isExisting) {
+      filtered.push({
+        key: inputValue,
+        addNewKey: inputValue,
+        label: `Add "${inputValue}"`,
+      });
+    }
+
+    return filtered;
+  }
+
+  function getOptionLabel(option: IOption): string {
+    return option.addNewKey && getSelectedAttributeValue() == option.addNewKey
+      ? option.addNewKey + " *"
+      : option.label;
+  }
+};
--- app/src/components/details/add/MetricAttributesSelector.tsx
+++ app/src/components/details/add/MetricAttributesSelector.tsx
@@ -1,10 +1,8 @@
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
 import React from "react";
-import { IMetricAttributes } from "../../../serverApi/IMetricAttributes";
 import { IMetricAttributeValues } from "../../../serverApi/IMetricAttributeValues";
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { IMetricAttribute } from "../../../serverApi/IMetricAttribute";
+import { MetricAttributeSelector } from "./MetricAttributeSelector";
+import { IMetricAttributes } from "../../../serverApi/IMetricAttributes";
 
 export const MetricAttributesSelector: React.FC<{
   attributes: IMetricAttributes;
@@ -17,36 +15,14 @@ export const MetricAttributesSelector: React.FC<{
         const attribute: IMetricAttribute = attributes[attributeKey];
 
         return (
-          <FormControl key={attributeKey}>
-            <InputLabel id={"metric-attribute-label-" + attributeKey}>
-              {attribute.name}
-            </InputLabel>
-            <Select
-              id={"metric-attribute-" + attributeKey}
-              labelId={"metric-attribute-label-" + attributeKey}
-              label={attribute.name}
-              value={selectedAttributeValues[attributeKey]?.[0] || ""}
-              onChange={(event: SelectChangeEvent) => {
-                onChange({
-                  ...selectedAttributeValues,
-                  ...{ [attributeKey]: [event.target.value] },
-                });
-              }}
-            >
-              <MenuItem key={""} value={""}>
-                -
-              </MenuItem>
-              {Object.entries(attribute.values).map(
-                (kvps: [key: string, value: string]) => {
-                  return (
-                    <MenuItem key={kvps[0]} value={kvps[0]}>
-                      {kvps[1]}
-                    </MenuItem>
-                  );
-                }
-              )}
-            </Select>
-          </FormControl>
+          <React.Fragment key={attributeKey}>
+            <MetricAttributeSelector
+              attributeKey={attributeKey}
+              attribute={attribute}
+              selectedAttributeValues={selectedAttributeValues}
+              onChange={onChange}
+            />
+          </React.Fragment>
         );
       })}
     </>
