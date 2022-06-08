29eb127641759076e31d2b9192141c4a765469b1
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-06-08 11:24:50 +0200

    Improvements

--- app/src/components/details/edit/MetricAttributeEditor.tsx
+++ app/src/components/details/edit/MetricAttributeEditor.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { IMetricAttribute } from "../../../serverApi/IMetricAttribute";
 import {
-  Button,
   Collapse,
   List,
   ListItem,
@@ -15,7 +14,6 @@ export const MetricAttributeEditor: React.FC<{
   attribute: IMetricAttribute;
   onChange: (attribute: IMetricAttribute) => void;
 }> = ({ attribute, onChange }) => {
-  const [showInput, setShowInput] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   return (
@@ -27,30 +25,67 @@ export const MetricAttributeEditor: React.FC<{
 
       <Collapse in={!isCollapsed} timeout="auto" unmountOnExit>
         <List dense={true}>
-          {Object.entries(attribute.values || {}).map(([key, value]) => {
-            return (
-              <ListItem key={key}>
-                <ListItemText>{value}</ListItemText>
-              </ListItem>
-            );
-          })}
+          {Object.entries(attribute.values || {}).map(([key, value]) => (
+            <ListItem key={key}>
+              <ListItemTextField
+                defaultValue={value}
+                isExisting={true}
+                onBlur={(newValue) => handleOnChange(key, newValue)}
+              />
+            </ListItem>
+          ))}
         </List>
 
-        <Button onClick={() => setShowInput(true)}>Add new value</Button>
-        {showInput ? (
-          <TextField
-            size="small"
-            onBlur={(event) => {
-              const newValue = event.target.value;
-
-              const updatedAttribute = { ...attribute };
-              updatedAttribute.values[newValue] = newValue;
-
-              return onChange(updatedAttribute);
-            }}
-          />
-        ) : null}
+        <ListItem key={"add_new_value"}>
+          <ListItemText>
+            <ListItemTextField
+              onBlur={(newValue: string) => handleOnChange(newValue, newValue)}
+            />
+          </ListItemText>
+        </ListItem>
       </Collapse>
     </>
+  );
+
+  function handleOnChange(attributeKey: string, value: string) {
+    const updatedAttribute = { ...attribute };
+    updatedAttribute.values[attributeKey] = value;
+
+    return onChange(updatedAttribute);
+  }
+};
+
+const ListItemTextField: React.FC<{
+  defaultValue?: string;
+  onBlur?: (newValue: string) => void;
+  isExisting?: boolean;
+}> = ({ defaultValue, onBlur, isExisting }) => {
+  const [disabled, setDisabled] = useState(isExisting);
+
+  const [value, setValue] = useState(defaultValue);
+
+  return (
+    <TextField
+      value={value}
+      disabled={disabled}
+      size="small"
+      onChange={(event) => setValue(event.target.value)}
+      onClick={() => setDisabled(false)}
+      onBlur={(event) => {
+        const newValue = event.target.value;
+        if (!newValue) {
+          return;
+        }
+
+        onBlur(newValue);
+
+        if (isExisting) {
+          setDisabled(true);
+          return;
+        }
+
+        setValue("");
+      }}
+    />
   );
 };
