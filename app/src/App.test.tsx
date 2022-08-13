12c0e7598d149bcf4c8bbd21ff5bdc1b24bd288b
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-08-14 00:03:57 +0200

    Fix test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,6 +2,15 @@ import { render } from "@testing-library/react";
 import { App } from "./App";
 
 test("placeholder test (that doesn't fail)", () => {
+  // IntersectionObserver isn't available in test environment
+  const mockIntersectionObserver = jest.fn();
+  mockIntersectionObserver.mockReturnValue({
+    observe: (): object => null,
+    unobserve: (): object => null,
+    disconnect: (): object => null,
+  });
+  window.IntersectionObserver = mockIntersectionObserver;
+
   render(
     <App
       user={{
