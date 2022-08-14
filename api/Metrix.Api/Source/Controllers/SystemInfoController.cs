9bb9c3bddc58c6d33e0643c3ffec0f7b8655e776
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-08-15 00:47:22 +0200

    Trying out pushing stuff in to Assembly

---
+++ api/Metrix.Api/Source/Controllers/SystemInfoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Metrix.Api.Controllers;
+
+[ApiController]
+[Route("api/system_info")]
+[AllowAnonymous] //remove
+public class SystemInfoController : Controller
+{
+  public SystemInfo Index()
+  {
+    var executingAssembly = Assembly.GetExecutingAssembly();
+    Version? version = executingAssembly.GetName().Version;
+
+    AssemblyInformationalVersionAttribute? customAttributeData = executingAssembly
+      .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute))
+      .FirstOrDefault() as AssemblyInformationalVersionAttribute;
+
+    return new SystemInfo
+    {
+      Version = version?.ToString(),
+      AdditionalStuff = customAttributeData.InformationalVersion,
+      BuildDateTime = DateTime.UtcNow
+    };
+  }
+}
+
+public class SystemInfo
+{
+  public string Version { get; set; }
+  public DateTime BuildDateTime { get; set; }
+  public string AdditionalStuff { get; set; }
+}
