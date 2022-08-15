95f8bc754fa0b8dbcc40491f7c42abdc3f87f2c0
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-08-15 14:06:33 +0200

    Date stuff...

--- api/Metrix.Api/Source/Controllers/SystemInfoController.cs
+++ api/Metrix.Api/Source/Controllers/SystemInfoController.cs
@@ -4,6 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Metrix.Api.Controllers;
 
+public class SystemInfo
+{
+  public string Version { get; set; }
+
+  public DateTime BuildDateTime { get; set; }
+
+  public string CommitHash { get; set; }
+  
+  public string ALL { get; set; }
+}
+
 [ApiController]
 [Route("api/system_info")]
 [AllowAnonymous] //remove
@@ -12,24 +23,30 @@ public class SystemInfoController : Controller
   public SystemInfo Index()
   {
     var executingAssembly = Assembly.GetExecutingAssembly();
-    Version? version = executingAssembly.GetName().Version;
 
     AssemblyInformationalVersionAttribute? customAttributeData = executingAssembly
       .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute))
       .FirstOrDefault() as AssemblyInformationalVersionAttribute;
 
+    if (customAttributeData == null)
+    {
+      // local/dev mode
+      return new SystemInfo()
+      {
+        Version = "42",
+        CommitHash = "78c0eab8a6ac0ab631cd93a3e41dd8c5ff5e116f",
+        BuildDateTime = DateTime.UtcNow.AddYears(37),
+      };
+    }
+
+    string[] segments = customAttributeData.InformationalVersion.Split("+");
+
     return new SystemInfo
     {
-      Version = version?.ToString(),
-      AdditionalStuff = customAttributeData.InformationalVersion,
+      ALL = customAttributeData.InformationalVersion,
+      Version = segments[0],
+      CommitHash = segments[1],
       BuildDateTime = DateTime.UtcNow
     };
   }
-}
-
-public class SystemInfo
-{
-  public string Version { get; set; }
-  public DateTime BuildDateTime { get; set; }
-  public string AdditionalStuff { get; set; }
 }
