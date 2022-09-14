8ac11684c14320928737dd663fee5dbc9ca8fce6
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-09-15 00:28:37 +0200

    DateRange

--- app/src/components/details/chart/dateSelection/DateConditions.tsx
+++ app/src/components/details/chart/dateSelection/DateConditions.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { DateTimeSelector } from "../../../common/DateTimeSelector";
 import { useMetricDetailsContext } from "../../MetricDetailsContext";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { calculateDateRange } from "./calculateDateRange";
+
+export enum DateRange {
+  All,
+  Week,
+  Month,
+  Year,
+}
 
 export const DateConditions: React.FC = () => {
   const { dateConditions, setDateConditions } = useMetricDetailsContext();
+
+  const [dateRange, setDateRange] = useState<DateRange>(DateRange.Month);
 
   return (
     <>
@@ -21,6 +38,30 @@ export const DateConditions: React.FC = () => {
           setDateConditions({ ...dateConditions, to: d });
         }}
       />
+      <FormControl margin="normal">
+        <InputLabel id="date-range-label">Date Range</InputLabel>
+        <Select
+          id="date-range"
+          labelId="date-range-label"
+          label="Date Range"
+          value={dateRange as unknown as string}
+          onChange={(event: SelectChangeEvent) => {
+            onChange(event.target.value as unknown as DateRange);
+          }}
+        >
+          <MenuItem value={DateRange.All}>All</MenuItem>
+          <MenuItem value={DateRange.Week}>Week</MenuItem>
+          <MenuItem value={DateRange.Month}>Month</MenuItem>
+          <MenuItem value={DateRange.Year}>Year</MenuItem>
+        </Select>
+      </FormControl>
     </>
   );
+
+  function onChange(range: DateRange): void {
+    setDateRange(range);
+    setDateConditions(
+      calculateDateRange(range, dateConditions.from ?? new Date())
+    );
+  }
 };
---
+++ app/src/components/details/chart/dateSelection/calculateDateRange.ts
@@ -0,0 +1,32 @@
+import { DateRange } from "./DateConditions";
+import { IDateConditions } from "../../MetricDetailsContext";
+import {
+  endOfMonth,
+  endOfWeek,
+  endOfYear,
+  startOfMonth,
+  startOfWeek,
+  startOfYear,
+} from "date-fns";
+
+export const calculateDateRange = (
+  dateRange: DateRange,
+  date: Date
+): IDateConditions => {
+  switch (dateRange) {
+    case DateRange.All:
+      return {};
+    case DateRange.Week:
+      return {
+        from: startOfWeek(date),
+        to: endOfWeek(date),
+      };
+    case DateRange.Month:
+      return {
+        from: startOfMonth(date),
+        to: endOfMonth(date),
+      };
+    case DateRange.Year:
+      return { from: startOfYear(date), to: endOfYear(date) };
+  }
+};
