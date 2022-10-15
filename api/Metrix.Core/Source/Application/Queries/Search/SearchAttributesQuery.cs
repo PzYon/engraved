22a1729ef8e99e9aee4dcfd7677e274ce3aec041
Markus Doggweiler <markus.doggweiler@gmail.com>
2022-10-16 00:01:15 +0200

    Add SearchAttributesQuery

---
+++ api/Metrix.Core/Source/Application/Queries/Search/SearchAttributesQuery.cs
@@ -0,0 +1,39 @@
+﻿using Metrix.Core.Application.Search;
+
+namespace Metrix.Core.Application.Queries.Search;
+
+public class SearchAttributesQuery : IQuery<AttributeSearchResult[]>
+{
+  public string MetricId { get; set; }
+
+  public string SearchText { get; set; }
+
+  // hack - this stuff should be injected via DI
+  private ISearchIndex _searchIndex;
+  private Dispatcher _dispatcher;
+
+  public IQueryExecutor<AttributeSearchResult[]> CreateExecutor()
+  {
+    return new SearchAttributesQueryExecutor(this);
+  }
+
+  public void SetSearchIndex(ISearchIndex searchIndex)
+  {
+    _searchIndex = searchIndex;
+  }
+
+  public ISearchIndex GetSearchIndex()
+  {
+    return _searchIndex;
+  }
+
+  public void SetDispatcher(Dispatcher dispatcher)
+  {
+    _dispatcher = dispatcher;
+  }
+
+  public Dispatcher GetDispatcher()
+  {
+    return _dispatcher;
+  }
+}
