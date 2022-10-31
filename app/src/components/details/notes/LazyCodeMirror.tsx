1ac082fea74079f2b0db80a362eeffb93a690c8a
Markus Doggweiler <PzYon@users.noreply.github.com>
2022-10-31 15:28:17 +0100

    Several minor improvements for notes (#397)
    * Reload note after save
    * Lazy load CodeMirror
    * A little bit of styling

---
+++ app/src/components/details/notes/LazyCodeMirror.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown } from "@codemirror/lang-markdown";
+import { EditorView } from "@codemirror/view";
+import { ICodeMirrorProps } from "./MarkdownEditor";
+
+const LazyCodeMirror: React.FC<ICodeMirrorProps> = ({
+  value,
+  onChange,
+  theme,
+}) => {
+  return (
+    <CodeMirror
+      value={value}
+      extensions={[markdown({}), EditorView.lineWrapping]}
+      onChange={onChange}
+      theme={EditorView.theme({
+        "&": {
+          fontSize: theme.typography.fontSize + "px",
+          backgroundColor: theme.palette.common.white,
+        },
+        ".cm-focused": {
+          outline: 0,
+        },
+        ".cm-activeLine": {
+          backgroundColor: theme.palette.background.default,
+        },
+      })}
+    />
+  );
+};
+
+export default LazyCodeMirror;
