45acbae19216173acd72f3869249cd56c97d13eb
Markus Doggweiler <PzYon@users.noreply.github.com>
2023-02-06 13:35:29 +0100

    Improvements for dot pulsation (#612)
    * Improve timing of fader
    * Extract class
    * Ajudst hiding of HeaderActions
    * Make timing for fade stuff a constant

---
+++ app/src/serverApi/LoadingHandler.ts
@@ -0,0 +1,65 @@
+export class LoadingHandler {
+  delayMs = 700;
+
+  private _loadingCounter = 0;
+  private _interval: unknown;
+  private _currentState: "first" | "last" | "other";
+
+  private handlers: { [key: string]: (isLoading: boolean) => void } = {};
+
+  oneMore() {
+    this.updateCounter("oneMore");
+  }
+
+  oneLess() {
+    this.updateCounter("oneLess");
+  }
+
+  registerHandler(key: string, handler: (isLoading: boolean) => void): void {
+    this.handlers[key] = handler;
+  }
+
+  unregisterHandler(key: string): void {
+    delete this.handlers[key];
+  }
+
+  private callHandlers(loading: boolean) {
+    for (const key in this.handlers) {
+      this.handlers[key](loading);
+    }
+  }
+
+  private updateCounter(direction: "oneMore" | "oneLess") {
+    const diff = direction == "oneMore" ? 1 : -1;
+    this._loadingCounter = this._loadingCounter + diff;
+
+    this._currentState = this.getCurrentState(direction);
+
+    if (this._currentState !== "first") {
+      return;
+    }
+
+    this.callHandlers(true);
+
+    this._interval = setInterval(() => {
+      if (this._currentState !== "last") {
+        return;
+      }
+
+      clearInterval(this._interval as never);
+      this.callHandlers(false);
+    }, this.delayMs);
+  }
+
+  private getCurrentState(direction: "oneMore" | "oneLess") {
+    if (this._loadingCounter === 1 && direction === "oneMore") {
+      return "first";
+    }
+
+    if (this._loadingCounter === 0 && direction === "oneLess") {
+      return "last";
+    }
+
+    return "other";
+  }
+}
--- app/src/serverApi/ServerApi.ts
+++ app/src/serverApi/ServerApi.ts
@@ -22,19 +22,14 @@ import { IAttributeSearchResult } from "./IAttributeSearchResult";
 import { IThresholdValues } from "./IThresholdValues";
 import { IMetricThresholds } from "./IMetricThresholds";
 import { IMetricUiSettings } from "../components/details/edit/MetricUiSettings";
+import { LoadingHandler } from "./LoadingHandler";
 
 type HttpMethod = "GET" | "PUT" | "POST" | "DELETE";
 
 export class ServerApi {
   private static _jwtToken: string;
-  private static _loadingCounter = 0;
-  private static _timer: unknown;
 
-  private static onLoadingToggle: (loading: boolean) => void;
-
-  static registerOnLoadingToggle(onToggle: (loading: boolean) => void): void {
-    ServerApi.onLoadingToggle = onToggle;
-  }
+  static loadingHandler: LoadingHandler = new LoadingHandler();
 
   static async wakeMeUp(): Promise<void> {
     return await this.executeRequest<void>("/wake/me/up");
@@ -215,7 +210,7 @@ export class ServerApi {
     payload: unknown = undefined
   ): Promise<T> {
     try {
-      ServerApi.updateCounter("oneMore");
+      ServerApi.loadingHandler.oneMore();
 
       const start = performance.now();
 
@@ -232,7 +227,7 @@ export class ServerApi {
 
       throw new ApiError(response.status, json as IApiError);
     } finally {
-      ServerApi.updateCounter("oneLess");
+      ServerApi.loadingHandler.oneLess();
     }
   }
 
@@ -259,27 +254,6 @@ export class ServerApi {
       new Request(envSettings.apiBaseUrl + url),
       requestConfig
     );
-  }
-
-  private static updateCounter(direction: "oneMore" | "oneLess") {
-    const diff = direction == "oneMore" ? 1 : -1;
-    ServerApi._loadingCounter = ServerApi._loadingCounter + diff;
-    this.callOnLoadingToggle();
-  }
-
-  private static callOnLoadingToggle() {
-    clearTimeout(ServerApi._timer as never);
-
-    const isOver = ServerApi._loadingCounter === 0;
-
-    if (!isOver) {
-      ServerApi.onLoadingToggle?.(true);
-      return;
-    }
-
-    ServerApi._timer = setTimeout(() => {
-      ServerApi.onLoadingToggle?.(false);
-    }, 700);
   }
 
   private static printPerfData(
