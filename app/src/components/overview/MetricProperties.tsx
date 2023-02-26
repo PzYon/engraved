ba2864caa2e0642bf135577b0c89457b436a16dc
Markus Doggweiler <PzYon@users.noreply.github.com>
2023-02-26 23:23:37 +0100

    Show "shared with" in overview (#655)

--- app/src/components/overview/MetricProperties.tsx
+++ app/src/components/overview/MetricProperties.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IMetric } from "../../serverApi/IMetric";
-import { styled, Typography } from "@mui/material";
+import { Avatar, AvatarGroup, styled, Typography } from "@mui/material";
 import { MetricTypeFactory } from "../../metricTypes/MetricTypeFactory";
 import { IMetricOverviewPropertyDefinition } from "../../metricTypes/IMetricType";
 import { FormatDate } from "../common/FormatDate";
@@ -15,21 +15,43 @@ export const MetricProperties: React.FC<{ metric: IMetric }> = ({ metric }) => {
       node: <FormatDate value={metric.editedOn} />,
       label: "Edited",
     },
+    {
+      key: "shared-with",
+      node: (
+        <AvatarGroup max={4} sx={{ display: "inline-flex" }}>
+          {Object.keys(metric.permissions).map((i) => {
+            const u = metric.permissions[i].user;
+            return (
+              <Avatar
+                key={u.name}
+                alt={u.displayName}
+                src={u.imageUrl}
+                sx={{ width: "15px", height: "15px" }}
+              />
+            );
+          })}
+        </AvatarGroup>
+      ),
+      hideWhen: () => !Object.keys(metric.permissions).length,
+      label: "Shared with",
+    },
     ...properties,
   ];
 
   return (
     <>
-      {allProperties.map((p) => (
-        <Property key={p.key}>
-          <Typography component="span" sx={{ fontWeight: "200" }}>
-            {p.label}
-          </Typography>{" "}
-          <Typography sx={{ color: "primary.main" }} component="span">
-            {p.node}
-          </Typography>
-        </Property>
-      ))}
+      {allProperties
+        .filter((p) => !p.hideWhen || !p.hideWhen())
+        .map((p) => (
+          <Property key={p.key}>
+            <Typography component="span" sx={{ fontWeight: "200" }}>
+              {p.label}
+            </Typography>{" "}
+            <Typography sx={{ color: "primary.main" }} component="span">
+              {p.node}
+            </Typography>
+          </Property>
+        ))}
     </>
   );
 };
