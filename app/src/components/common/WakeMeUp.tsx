b3d00ffc6423739074a2fb71e7539d59dcd21ab2
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-03-08 00:16:56 +0100

    Make WakeMeUp use authenticated API and rotate

--- app/src/components/common/WakeMeUp.tsx
+++ app/src/components/common/WakeMeUp.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { SyncOutlined } from "@mui/icons-material";
 import { IconButtonWrapper } from "./IconButtonWrapper";
 import { ServerApi } from "../../serverApi/ServerApi";
+import { styled } from "@mui/material";
 
 const tenMinutes = 10 * 60 * 1000;
 
 export const WakeMeUp: React.FC = () => {
   const [showMe, setShowMe] = useState(false);
+  const [doRotate, setDoRotate] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setShowMe(true), tenMinutes);
@@ -17,16 +19,37 @@ export const WakeMeUp: React.FC = () => {
   }
 
   return (
-    <IconButtonWrapper
-      action={{
-        icon: <SyncOutlined />,
-        onClick: () => {
-          ServerApi.wakeMeUp().then(() => setShowMe(false));
-        },
-        label: "Wake up server",
-        key: "wake_me_up",
-        sx: { color: "common.white" },
-      }}
-    />
+    <Host className={doRotate ? "rotate" : null}>
+      <IconButtonWrapper
+        action={{
+          icon: <SyncOutlined />,
+          onClick: async () => {
+            setDoRotate(true);
+            await ServerApi.getSystemInfo();
+            setDoRotate(false);
+            setShowMe(false);
+          },
+          label: "Wake up server",
+          key: "wake_me_up",
+          sx: { color: "common.white" },
+        }}
+      />
+    </Host>
   );
 };
+
+const Host = styled("span")`
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+
+  &.rotate {
+    animation: rotation ${(ServerApi.loadingHandler.delayMs * 2) / 1000}s
+      infinite;
+  }
+`;
