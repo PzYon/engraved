5fffe0863b63d143f37db054c07132179be4df0e
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-03-21 22:09:59 +0100

    Transfer

--- app/src/components/common/FadeInContainer.tsx
+++ app/src/components/common/FadeInContainer.tsx
@@ -1,24 +1,22 @@
-import { CSSProperties, ReactNode, useEffect, useRef } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { styled } from "@mui/material";
 
-export const FadeInContainer = (props: {
-  style?: CSSProperties;
+export const FadeInContainer: React.FC<{
   children: ReactNode;
-}) => {
-  const containerEl = useRef<HTMLDivElement>();
+}> = ({ children }) => {
+  const [isRendered, setIsRendered] = useState(false);
 
   useEffect(() => {
-    containerEl.current.style.opacity = "1";
+    setIsRendered(true);
   }, []);
 
   return (
-    <ContainerSection style={props.style} ref={containerEl}>
-      {props.children}
+    <ContainerSection style={{ opacity: isRendered ? 1 : 0 }}>
+      {children}
     </ContainerSection>
   );
 };
 
 const ContainerSection = styled("section")`
-  opacity: 0;
   transition: opacity 700ms;
 `;
