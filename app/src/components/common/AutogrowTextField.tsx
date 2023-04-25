4292ffbd51b586d9344134ac8a9ea76bf6a76560
Markus Doggweiler <PzYon@users.noreply.github.com>
2023-04-26 00:28:03 +0200

    Scrap text field and alignment of checkboxes (#806)

---
+++ app/src/components/common/AutogrowTextField.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { TextFieldProps } from "@mui/material/TextField/TextField";
+import { styled, TextField } from "@mui/material";
+
+export const AutogrowTextField: React.FC<
+  TextFieldProps & { fieldType: "title" | "content" }
+> = (props) => {
+  const TF =
+    props.fieldType === "title" ? StyledTitleTextField : StyledTextField;
+
+  return (
+    <TF
+      variant="standard"
+      multiline={true}
+      autoComplete={"new-password"}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+        }
+      }}
+      {...props}
+    />
+  );
+};
+
+const StyledTextField = styled(TextField)`
+  width: 100%;
+
+  textarea {
+    &,
+    &.Mui-disabled {
+      -webkit-text-fill-color: ${(p) =>
+        p.theme.palette.text.primary} !important;
+      color: ${(p) => p.theme.palette.text.primary} !important;
+    }
+  }
+
+  .MuiInputBase-root {
+    padding: 0;
+  }
+
+  .MuiInput-root:before {
+    border: 0 !important;
+    outline: 0 !important;
+  }
+
+  fieldset {
+    border-width: 0;
+  }
+`;
+
+const StyledTitleTextField = styled(StyledTextField)`
+  textarea {
+    &,
+    &.Mui-disabled {
+    -webkit-text-fill-color: ${(p) => p.theme.palette.primary.main} !important;
+    color: ${(p) => p.theme.palette.primary.main} !important;
+      font-size: larger;
+  }
+`;
