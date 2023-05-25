f40e400bdde51e3c780204e346d5823a1077d520
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-05-25 22:59:10 +0200

    Styling

--- app/src/components/details/add/renderAddQuickScrapDialog.tsx
+++ app/src/components/details/add/renderAddQuickScrapDialog.tsx
@@ -1,11 +1,6 @@
 import { IDialogProps } from "../../layout/dialogs/DialogContext";
 import { IUser } from "../../../serverApi/IUser";
-import { ScrapsMetricType } from "../../../metricTypes/ScrapsMetricType";
-import { Scrap } from "../scraps/Scrap";
-import React, { useState } from "react";
-import { styled, ToggleButton, ToggleButtonGroup } from "@mui/material";
-import { ListScrapIcon, MarkdownScrapIcon } from "../scraps/ScrapsViewPage";
-import { ScrapType } from "../../../serverApi/IScrapMeasurement";
+import { AddQuickScrapDialog } from "./addQuickScrapDialog";
 
 export const renderAddQuickScrapDialog = (
   user: IUser,
@@ -21,37 +16,3 @@ export const renderAddQuickScrapDialog = (
     ),
   });
 };
-
-export const AddQuickScrapDialog: React.FC<{
-  onSuccess?: () => void;
-  metricId: string;
-}> = ({ onSuccess, metricId }) => {
-  const [type, setType] = useState<ScrapType>(ScrapType.Markdown);
-
-  const scrap = ScrapsMetricType.createBlank(metricId, type);
-
-  return (
-    <>
-      <ScrapTypeSelector>
-        <ToggleButtonGroup
-          value={type}
-          exclusive
-          onChange={(_, value) => setType(value)}
-        >
-          <ToggleButton value={ScrapType.Markdown}>
-            <MarkdownScrapIcon />
-          </ToggleButton>
-          <ToggleButton value={ScrapType.List}>
-            <ListScrapIcon />
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </ScrapTypeSelector>
-      <Scrap scrap={scrap} hideDate={true} onSuccess={onSuccess} />
-    </>
-  );
-};
-
-const ScrapTypeSelector = styled("div")`
-  display: flex;
-  justify-content: end;
-`;
