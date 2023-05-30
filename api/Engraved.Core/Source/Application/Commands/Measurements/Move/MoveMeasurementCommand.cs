b8d2d1f622cd4bba0a925243e27845c31594012c
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-05-31 00:05:19 +0200

    MoveMeasurementCommandExecutor

---
+++ api/Engraved.Core/Source/Application/Commands/Measurements/Move/MoveMeasurementCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Engraved.Core.Application.Commands.Measurements.Move;
+
+public class MoveMeasurementCommand : ICommand
+{
+  public string MeasurementId { get; set; } = null!;
+  
+  public string TargetMetricId { get; set; } = null!;
+
+  public ICommandExecutor CreateExecutor()
+  {
+    return new MoveMeasurementCommandExecutor(this);
+  }
+}
