c90d026b936a54cf1b1da944270ab8c1d7556573
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-06-28 14:34:09 +0200

    Give SearchBox autoFocus

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// we want a separate chunk for the envSettings stuff in
+// order to be able to reload it independent of the other
+// chunks. this enables us to detect, if a new version
+// has been deployed.
+const envSettings = "envSettings";
+
 export default () => {
   return defineConfig({
     server: {
@@ -11,13 +17,13 @@ export default () => {
       rollupOptions: {
         output: {
           chunkFileNames: (chunkInfo) => {
-            return chunkInfo.name === "envSettings"
+            return chunkInfo.name === envSettings
               ? `[name].js`
               : `[name].[hash].js`;
           },
           manualChunks: (id) => {
-            if (id.includes("envSettings")) {
-              return "envSettings";
+            if (id.includes(envSettings)) {
+              return envSettings;
             }
           },
         },
