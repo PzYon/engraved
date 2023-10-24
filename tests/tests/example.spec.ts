a00f73f9f2180e1e19c5dce0191abda51cb0cbc0
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-10-24 08:54:10 +0200

    Try fix tests

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,8 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.waitForTimeout(20000);
-
   // wait for server API
   await page.goto("https://localhost:7072", {timeout: 60000});
 
