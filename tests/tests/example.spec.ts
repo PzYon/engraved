5298c3034bc0ba1105449e3fefb8cc14c6e88816
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-10-24 09:47:24 +0200

    Again...

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,8 +1,10 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
+  await page.waitForTimeout(30000);
+
   // wait for server API
-  await page.goto("http://localhost:5072", { timeout: 60000 });
+  await page.goto("http://localhost:5072");
 
   // load client
   await page.goto("http://localhost:3000/");
