481a5b8bf20eebffb857984d35d9217b37ac5d40
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-10-24 22:46:09 +0200

    Again...

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -5,4 +5,19 @@ test("has title", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
   await expect(page).toHaveTitle(/engraved/);
+
+  const pagePromise = page.waitForEvent("popup");
+  await page
+    .frameLocator('iframe[title="Schaltfläche „Über Google anmelden“"]')
+    .getByLabel("Über Google anmelden")
+    .click();
+
+  const loginPopup = await pagePromise;
+  await loginPopup.getByLabel("Email or phone").click();
+  await loginPopup.getByLabel("Email or phone").fill("");
+  await loginPopup.getByLabel("Email or phone").press("Enter");
+
+  await loginPopup.getByLabel("Enter your password").click();
+
+  await loginPopup.getByLabel("Enter your password").fill("");
 });
