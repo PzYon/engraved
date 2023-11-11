3ffd8707a39be1ad00edbddf45f1cf38dd7fc917
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-11-11 23:40:26 +0100

    Run tests

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1, //process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : [["list"], ["html"]],
+  reporter: process.env.CI ? [["github"], ["list"]] : [["list"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -65,10 +65,21 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+      command: "npm run e2e:start-app",
+      url: "http://localhost:3000",
+      reuseExistingServer: !process.env.CI,
+      stdout: "ignore",
+      stderr: "pipe",
+    },
+    {
+      command: "npm run e2e:start-api",
+      url: "http://localhost:5072",
+      reuseExistingServer: !process.env.CI,
+      stdout: "ignore",
+      stderr: "pipe",
+      ignoreHTTPSErrors: true,
+    },
+  ],
 });
