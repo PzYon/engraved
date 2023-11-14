dd261211188177feb0608bce1d810031b2debc84
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-11-15 00:41:25 +0100

    Tests for JournalQueryUtil

---
+++ api/Engraved.Core.Tests/Source/Application/Queries/Journals/JournalQueryUtilShould.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Engraved.Core.Application.Persistence;
+using Engraved.Core.Application.Persistence.Demo;
+using Engraved.Core.Domain.Journals;
+using Engraved.Core.Domain.Permissions;
+using Engraved.Core.Domain.User;
+using NUnit.Framework;
+
+namespace Engraved.Core.Application.Queries.Journals;
+
+public class JournalQueryUtilShould
+{
+  private InMemoryRepository _testRepository = null!;
+  private UserScopedInMemoryRepository _userScopedInMemoryRepository = null!;
+
+  private string _meUserId = null!;
+  private string _youUserId = null!;
+
+  [SetUp]
+  public async Task SetUp()
+  {
+    _testRepository = new InMemoryRepository();
+
+    UpsertResult meUpsertResult = await _testRepository.UpsertUser(new User { Name = "me" });
+    _meUserId = meUpsertResult.EntityId;
+
+    UpsertResult youUpsertResult = await _testRepository.UpsertUser(new User { Name = "you" });
+    _youUserId = youUpsertResult.EntityId;
+
+    _userScopedInMemoryRepository = new UserScopedInMemoryRepository(
+      _testRepository,
+      new FakeCurrentUserService("me")
+    );
+  }
+
+  [Test]
+  public async Task SetUserRoleToOwner()
+  {
+    IJournal[] ensuredJournals = await JournalQueryUtil.EnsurePermissionUsers(
+      _userScopedInMemoryRepository,
+      new TimerJournal { UserId = _meUserId }
+    );
+
+    Assert.AreEqual(1, ensuredJournals.Length);
+    Assert.AreEqual(UserRole.Owner, ensuredJournals[0].UserRole);
+  }
+
+  [Test]
+  public async Task SetUserRoleToReader()
+  {
+    IJournal[] journals =
+    {
+      new TimerJournal
+      {
+        UserId = _youUserId,
+        Permissions = new UserPermissions
+        {
+          {
+            _meUserId,
+            new PermissionDefinition { Kind = PermissionKind.Read, User = new User { Id = _meUserId } }
+          }
+        }
+      }
+    };
+
+    IJournal[] ensuredJournals = await JournalQueryUtil.EnsurePermissionUsers(
+      _userScopedInMemoryRepository,
+      journals
+    );
+
+    Assert.AreEqual(1, ensuredJournals.Length);
+    Assert.AreEqual(UserRole.Reader, ensuredJournals[0].UserRole);
+  }
+}
