771235a74333a3f2fa2634bbfbdb2f73fcbd0e8b
Markus Doggweiler <PzYon@users.noreply.github.com>
2023-11-21 16:59:57 +0100

    Fix Properties padding in mobile view (#1360)

--- app/src/components/common/Properties.tsx
+++ app/src/components/common/Properties.tsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { IPropertyDefinition } from "./IPropertyDefinition";
 import { styled, Typography } from "@mui/material";
+import { DeviceWidth, useDeviceWidth } from "./useDeviceWidth";
 
 export const Properties: React.FC<{ properties: IPropertyDefinition[] }> = ({
   properties,
-}) => (
-  <Host>
-    {properties
-      .filter((p) => !p.hideWhen || !p.hideWhen())
-      .map((p) => (
-        <span className="ngrvd-property" key={p.key}>
-          <Property as="span">
-            {p.label ? (
-              <Typography component="span" sx={{ fontWeight: "200" }}>
-                {p.label}{" "}
-              </Typography>
-            ) : null}
+}) => {
+  const deviceWidth = useDeviceWidth();
 
-            <Typography sx={{ color: "primary.main" }} component="span">
-              {p.node}
-            </Typography>
-          </Property>
-        </span>
-      ))}
-  </Host>
-);
+  return (
+    <Host sx={{ p: deviceWidth === DeviceWidth.Small ? 2 : 0 }}>
+      {properties
+        .filter((p) => !p.hideWhen || !p.hideWhen())
+        .map((p) => (
+          <span className="ngrvd-property" key={p.key}>
+            <Property as="span">
+              {p.label ? (
+                <Typography component="span" sx={{ fontWeight: "200" }}>
+                  {p.label}
+                </Typography>
+              ) : null}
+
+              <Typography component="span" sx={{ color: "primary.main" }}>
+                {p.node}
+              </Typography>
+            </Property>
+          </span>
+        ))}
+    </Host>
+  );
+};
 
 const Host = styled("div")`
   .ngrvd-property {
