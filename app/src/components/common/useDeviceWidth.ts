40dec68f6fdcfb8033bfe3f0d0a9159debc0bb5e
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-12-02 21:02:26 +0100

    Fix table layout on small devices

--- app/src/components/common/useDeviceWidth.ts
+++ app/src/components/common/useDeviceWidth.ts
@@ -3,23 +3,12 @@ import { useMediaQuery } from "@mui/material";
 export enum DeviceWidth {
   Small,
   Normal,
-  Large,
 }
 
-export const maxWidthInPx = 1200;
 export const minWidthInPx = 600;
 
 export const useDeviceWidth = (): DeviceWidth => {
-  const isLarge = useMediaQuery(`(min-width:${maxWidthInPx}px)`);
-  const isNormal = useMediaQuery(`(min-width:${minWidthInPx}px)`);
-
-  if (isLarge) {
-    return DeviceWidth.Large;
-  }
-
-  if (isNormal) {
-    return DeviceWidth.Normal;
-  }
-
-  return DeviceWidth.Small;
+  return useMediaQuery(`(min-width:${minWidthInPx}px)`)
+    ? DeviceWidth.Normal
+    : DeviceWidth.Small;
 };
