25afc11735419677ebaa64c5a330c23ea798561e
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-12-11 23:27:37 +0100

    Fix useUpsertEntryMutation.ts

--- tests/src/poms/scrapsJournalPage.ts
+++ tests/src/poms/scrapsJournalPage.ts
@@ -1,6 +1,5 @@
 import { expect } from "@playwright/test";
 import { JournalPage } from "./journalPage";
-
 import { ScrapListComponent } from "./scrapListComponent";
 
 export class ScrapsJournalPage extends JournalPage {
@@ -10,6 +9,11 @@ export class ScrapsJournalPage extends JournalPage {
 
   async addList() {
     await this.clickPageAction("Add list");
+    return new ScrapListComponent(this.page);
+  }
+
+  async getListByTitle(title: string) {
+    throw new Error("Not yet implemented stuff here: " + title);
     return new ScrapListComponent(this.page);
   }
 }
--- tests/tests/scrapsList.spec.ts
+++ tests/tests/scrapsList.spec.ts
@@ -3,16 +3,15 @@ import { addNewJournal } from "../src/utils/addNewJournal";
 import { login } from "../src/utils/login";
 import { ScrapsJournalPage } from "../src/poms/scrapsJournalPage";
 
-test.beforeEach(async ({ page }) => {
-  await login(page, "scrapsList");
-});
-
 const firstItemText = "My First Item";
 const secondItemText = "My Second Item";
+const thirdItemText = "My Third Item";
 
 test("add scrap journal, add list entry and add/delete/modify", async ({
   page,
 }) => {
+  await login(page, "scrapsList");
+
   await addNewJournal(page, "Scraps", "My First Scraps Journal");
 
   const scrapsJournalPage = new ScrapsJournalPage(page);
@@ -37,7 +36,39 @@ test("add scrap journal, add list entry and add/delete/modify", async ({
     .getByRole("checkbox")
     .check();
 
-  // todo: edit, add again, validate
+  // todo:
+  // - edit, add again, validate
+  // - mark as checked in non-edit mode
 
   await scrapList.clickSave(true);
+});
+
+test("MULTIPLE WINDOWS", async ({ browser }) => {
+  const context = await browser.newContext();
+  const pageTab1 = await context.newPage();
+  await login(pageTab1, "background-update-joe");
+
+  await addNewJournal(pageTab1, "Scraps", "Simple Scrap");
+
+  const scrapsJournalPageTab1 = new ScrapsJournalPage(pageTab1);
+
+  const scrapListTab1 = await scrapsJournalPageTab1.addList();
+  await scrapListTab1.typeTitle("I is list");
+  await scrapListTab1.addListItem(firstItemText);
+  await scrapListTab1.addListItem(secondItemText);
+  await scrapListTab1.clickSave();
+
+  // second
+  const pageTab2 = await context.newPage();
+  await pageTab2.goto(pageTab1.url());
+
+  const scrapsJournalPageTab2 = new ScrapsJournalPage(pageTab2);
+  const scrapListTab2 = await scrapsJournalPageTab2.getListByTitle("I is list");
+  await scrapListTab2.dblClickToEdit();
+  await scrapListTab2.addListItem(thirdItemText);
+  await scrapListTab2.clickSave(true);
+
+  await pageTab1.evaluate(() => {
+    document.dispatchEvent(new Event("visibilitychange"));
+  });
 });
