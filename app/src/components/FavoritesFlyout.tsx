875cf4964b144d805e97d36dc0d42a1dfe9cb677
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-01-31 15:56:58 +0100

    Improvements

--- app/src/components/FavoritesFlyout.tsx
+++ app/src/components/FavoritesFlyout.tsx
@@ -1,10 +1,10 @@
 import { Star } from "@mui/icons-material";
-import { List, ListItemText, Popover } from "@mui/material";
+import { Menu, MenuItem } from "@mui/material";
 import React, { useRef, useState } from "react";
 import { useJournalsQuery } from "../serverApi/reactQuery/queries/useJournalsQuery";
-import { JournalMenuItem } from "./JournalMenuItem";
 import { Link } from "react-router-dom";
 import { ActionIconButton } from "./common/actions/ActionIconButton";
+import { JournalMenuItem } from "./JournalMenuItem";
 
 export const FavoritesFlyout: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,26 +27,20 @@ export const FavoritesFlyout: React.FC = () => {
           key: "favorites",
         }}
       ></ActionIconButton>
-      <Popover
-        open={isOpen}
-        anchorEl={anchorElement.current}
-        onClose={() => setIsOpen(false)}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-      >
-        <FavoritesList close={() => setIsOpen(false)} />
-      </Popover>
+      {isOpen ? (
+        <FavoritesList
+          anchorElement={anchorElement.current}
+          close={() => setIsOpen(false)}
+        />
+      ) : null}
     </>
   );
 };
 
-const FavoritesList: React.FC<{ close: () => void }> = ({ close }) => {
+const FavoritesList: React.FC<{
+  close: () => void;
+  anchorElement: Element;
+}> = ({ close, anchorElement }) => {
   const journals = useJournalsQuery(null, [], true);
 
   if (!journals?.length) {
@@ -54,19 +48,19 @@ const FavoritesList: React.FC<{ close: () => void }> = ({ close }) => {
   }
 
   return (
-    <List sx={{ p: 1 }}>
+    <Menu anchorEl={anchorElement} open={true} onClose={close}>
       {journals.map((journal) => {
         return (
-          <ListItemText sx={{ p: 1 }} key={journal.id}>
+          <MenuItem key={journal.id}>
             <Link to={`/journals/${journal.id}`} onClick={close}>
               <JournalMenuItem
                 journalType={journal.type}
                 label={journal.name}
               />
             </Link>
-          </ListItemText>
+          </MenuItem>
         );
       })}
-    </List>
+    </Menu>
   );
 };
