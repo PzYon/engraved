d2351532451ea73e9bf9c2b2040ce0938ad7356d
Markus Doggweiler <PzYon@users.noreply.github.com>
2024-02-05 00:18:24 +0100

    Fix react warning in DeleteEntryLauncher (#1621)

---
+++ app/src/components/details/edit/DeleteEntry.tsx
@@ -0,0 +1,35 @@
+import { IJournal } from "../../../serverApi/IJournal";
+import { useDeleteEntryMutation } from "../../../serverApi/reactQuery/mutations/useDeleteEntryMutation";
+import { DeleteButtons } from "../../common/DeleteButtons";
+import { JournalType } from "../../../serverApi/JournalType";
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const DeleteEntry: React.FC<{
+  journal: IJournal;
+  entryId: string;
+  closeDialog: () => void;
+}> = ({ journal, entryId, closeDialog }) => {
+  const deleteEntryMutation = useDeleteEntryMutation(journal.id, entryId);
+
+  return (
+    <>
+      <Typography>
+        Are you sure you want to delete this <b>scrap</b>? You will not be able
+        to recover it.
+      </Typography>
+      <DeleteButtons
+        entityType={"entry"}
+        requiresConfirmation={journal.type === JournalType.Scraps}
+        onDelete={() => deleteEntry(closeDialog)}
+        onCancel={closeDialog}
+      />
+    </>
+  );
+
+  function deleteEntry(closeDialog: () => void) {
+    deleteEntryMutation.mutate();
+
+    closeDialog();
+  }
+};
--- app/src/components/details/edit/DeleteEntryLauncher.tsx
+++ app/src/components/details/edit/DeleteEntryLauncher.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect } from "react";
 import { IJournal } from "../../../serverApi/IJournal";
 import { useDialogContext } from "../../layout/dialogs/DialogContext";
-import { useParams } from "react-router";
 import { useNavigate } from "react-router-dom";
-import { Typography } from "@mui/material";
-import { useDeleteEntryMutation } from "../../../serverApi/reactQuery/mutations/useDeleteEntryMutation";
-import { DeleteButtons } from "../../common/DeleteButtons";
-import { JournalType } from "../../../serverApi/JournalType";
+import { DeleteEntry } from "./DeleteEntry";
+import { useParams } from "react-router";
 
 export const DeleteEntryLauncher: React.FC<{
   journal: IJournal;
@@ -16,38 +13,23 @@ export const DeleteEntryLauncher: React.FC<{
 
   const navigate = useNavigate();
 
-  const deleteEntryMutation = useDeleteEntryMutation(journal.id, entryId);
-
   useEffect(() => {
     renderDialog({
       title: "Delete Entry",
       render: (closeDialog) => {
         return (
-          <>
-            <Typography>
-              Are you sure you want to delete this <b>scrap</b>? You will not be
-              able to recover it.
-            </Typography>
-            <DeleteButtons
-              entityType={"entry"}
-              requiresConfirmation={journal.type === JournalType.Scraps}
-              onDelete={() => deleteEntry(closeDialog)}
-              onCancel={closeDialog}
-            />
-          </>
+          <DeleteEntry
+            journal={journal}
+            entryId={entryId}
+            closeDialog={closeDialog}
+          />
         );
       },
       onClose: () => {
         navigate(`/journals/${journal.id}`);
       },
     });
-
-    function deleteEntry(closeDialog: () => void) {
-      deleteEntryMutation.mutate();
-
-      closeDialog();
-    }
-  }, [deleteEntryMutation, journal.id, journal.type, navigate, renderDialog]);
+  }, [journal.id, journal.type, navigate, renderDialog]);
 
   return null;
 };
