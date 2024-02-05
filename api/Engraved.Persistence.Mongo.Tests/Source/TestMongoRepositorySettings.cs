f09712cacc10957bb18d717d2bfff21ac6e3c115
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-02-05 23:58:43 +0100

    Different port

--- api/Engraved.Persistence.Mongo.Tests/Source/TestMongoRepositorySettings.cs
+++ api/Engraved.Persistence.Mongo.Tests/Source/TestMongoRepositorySettings.cs
@@ -2,7 +2,7 @@
 
 public class TestMongoRepositorySettings : IMongoRepositorySettings
 {
-  public string MongoDbConnectionString => "mongodb://127.0.0.1:27017";
+  public string MongoDbConnectionString => "mongodb://127.0.0.1:27018";
   public string DatabaseName => "metrix_unit_test";
   public string JournalsCollectionName => "journals";
   public string EntriesCollectionName => "entries";
