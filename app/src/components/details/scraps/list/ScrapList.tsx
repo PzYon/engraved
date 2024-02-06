6f104aa4ca40ffaa589fa30115472b4c495a02b9
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-02-06 22:59:35 +0100

    Scrap list DnD (WIP)

--- app/src/components/details/scraps/list/ScrapList.tsx
+++ app/src/components/details/scraps/list/ScrapList.tsx
@@ -10,6 +10,20 @@ import {
 import { ActionIconButtonGroup } from "../../../common/actions/ActionIconButtonGroup";
 import { ListItemCollection } from "./ListItemCollection";
 import { ISCrapListItem } from "./IScrapListItem";
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragOverEvent,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 export const ScrapList: React.FC<{
   isEditMode: boolean;
@@ -35,6 +49,13 @@ export const ScrapList: React.FC<{
     }
   }, [isEditMode, listItemsCollection]);
 
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
   return (
     <Host
       key={isEditMode.toString()}
@@ -48,22 +69,36 @@ export const ScrapList: React.FC<{
         {!isEditMode && !listItemsCollection.items?.length ? (
           <Typography sx={{ opacity: 0.4 }}>No items yet.</Typography>
         ) : (
-          listItemsCollection.items.map((item, index) => (
-            <ScrapListItem
-              key={listItemsCollection.getReactKey(index)}
-              listItemsCollection={listItemsCollection}
-              index={index}
-              listItem={item}
-              isEditMode={isEditMode}
-              onChange={(updatedItem) => {
-                listItemsCollection.updateItem(index, updatedItem);
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={listItemsCollection.items.map((item, index) => ({
+                ...item,
+                id: listItemsCollection.getReactKey(index),
+              }))}
+              strategy={verticalListSortingStrategy}
+            >
+              {listItemsCollection.items.map((item, index) => (
+                <ScrapListItem
+                  key={listItemsCollection.getReactKey(index)}
+                  listItemsCollection={listItemsCollection}
+                  index={index}
+                  listItem={item}
+                  isEditMode={isEditMode}
+                  onChange={(updatedItem) => {
+                    listItemsCollection.updateItem(index, updatedItem);
 
-                if (!isEditMode) {
-                  onSave(getItemsAsJson(listItemsCollection.items));
-                }
-              }}
-            />
-          ))
+                    if (!isEditMode) {
+                      onSave(getItemsAsJson(listItemsCollection.items));
+                    }
+                  }}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
         )}
       </List>
       {isEditMode ? (
@@ -103,6 +138,20 @@ export const ScrapList: React.FC<{
       ) : null}
     </Host>
   );
+
+  function handleDragEnd(event: DragOverEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      debugger;
+      /*setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });*/
+    }
+  }
 };
 
 function getItemsAsJson(rawItems: ISCrapListItem[]) {
