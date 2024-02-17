146752370990a99e3d1d59eb5381d613917eb571
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-02-17 09:09:07 +0100

    Test (WIP)

--- app/src/serverApi/LoginHandler.ts
+++ app/src/serverApi/LoginHandler.ts
@@ -1,50 +1,54 @@
 export interface ICallbackServerFunction {
   fn: () => Promise<unknown>;
   callback: (result: unknown) => void;
-  callbackError: (error: unknown) => void;
+  callbackError: (error: Error) => void;
 }
 
 export class LoginHandler {
-  constructor(private login: () => Promise<void>) {}
-
   private loginInProcess = false;
-
   private callServerFns: ICallbackServerFunction[] = [];
+
+  constructor(private login: () => Promise<void>) {}
 
   async loginAndRetry<T>(callServer: () => Promise<T>): Promise<T> {
     if (this.loginInProcess) {
       return new Promise<T>((resolve, reject) => {
-        this.callServerFns.push({
-          fn: callServer,
-          callback: (value: unknown) => resolve(value as T),
-          callbackError: (e) => reject(e),
-        });
+        this.addCallbackFn(callServer, resolve, reject);
       });
     }
 
     this.loginInProcess = true;
 
-    return this.login()
-      .then(async () => {
-        const foo = await callServer();
+    return new Promise((resolve, reject) => {
+      this.addCallbackFn(callServer, resolve, reject);
 
-        for (const callServerFn of this.callServerFns) {
-          try {
-            const result = await callServerFn.fn();
-            callServerFn.callback(result);
-          } catch (e) {
-            callServerFn.callbackError(e);
+      this.login()
+        .then(async () => {
+          for (const callServerFn of this.callServerFns) {
+            try {
+              const result = await callServerFn.fn();
+              callServerFn.callback(result);
+            } catch (e) {
+              callServerFn.callbackError(e as Error);
+            }
           }
-        }
+        })
+        .finally(() => {
+          this.loginInProcess = false;
+          this.callServerFns = [];
+        });
+    });
+  }
 
-        return foo;
-      })
-      .catch((e) => {
-        return new Promise<T>((_, reject) => reject(e));
-      })
-      .finally(() => {
-        this.loginInProcess = false;
-        this.callServerFns = [];
-      });
+  private addCallbackFn<T>(
+    callServer: () => Promise<T>,
+    resolve: (value: T) => void,
+    reject: (reason?: Error) => void,
+  ) {
+    this.callServerFns.push({
+      fn: callServer,
+      callback: (value: unknown) => resolve(value as T),
+      callbackError: (e) => reject(e),
+    });
   }
 }
