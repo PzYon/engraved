bf039e233107a342f576ea02287e1ed42348fe11
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-02-29 00:38:02 +0100

    Cleanup

--- api/Engraved.Core/Source/Application/Dispatcher.cs
+++ api/Engraved.Core/Source/Application/Dispatcher.cs
@@ -8,50 +8,41 @@ using Newtonsoft.Json;
 
 namespace Engraved.Core.Application;
 
-public class Dispatcher
+public class Dispatcher(
+  ILogger<Dispatcher> logger,
+  IServiceProvider serviceProvider,
+  IUserScopedRepository repository,
+  QueryCache queryCache
+)
 {
-  private readonly ILogger? _logger;
-  private readonly IServiceProvider _serviceProvider;
-  private readonly IUserScopedRepository _repository;
-  private readonly QueryCache _queryCache;
-
-  public Dispatcher(
-    ILogger<Dispatcher>? logger,
-    IServiceProvider serviceProvider,
-    IUserScopedRepository repository,
-    QueryCache queryCache
-  )
-  {
-    _logger = logger;
-    _serviceProvider = serviceProvider;
-    _repository = repository;
-    _queryCache = queryCache;
-  }
-
   public async Task<TResult> Query<TResult, TQuery>(TQuery query) where TQuery : IQuery
   {
     return await Execute(
       () => ExecuteQuery<TResult, TQuery>(query),
-      $"Query {query.GetType().Name}",
       query
     );
   }
 
   private async Task<TResult> ExecuteQuery<TResult, TQuery>(TQuery query) where TQuery : IQuery
   {
-    var queryExecutor = _serviceProvider.GetService<IQueryExecutor<TResult, TQuery>>();
+    var queryExecutor = serviceProvider.GetService<IQueryExecutor<TResult, TQuery>>();
     if (queryExecutor == null)
     {
       throw new Exception($"No query executor registered for query of type {query.GetType()}");
     }
 
-    if (!queryExecutor.DisableCache && _queryCache.TryGetValue(queryExecutor, query, out TResult? cachedResult))
+    if (!queryExecutor.DisableCache && queryCache.TryGetValue(queryExecutor, query, out TResult? cachedResult))
     {
       return cachedResult!;
     }
 
     TResult result = await queryExecutor.Execute(query);
-    _queryCache.Set(queryExecutor, query, result);
+    queryCache.Set(queryExecutor, query, result);
+
+    if (typeof(TResult).IsArray)
+    {
+      logger.LogInformation($"{query.GetType().Name}: Found {(result as Array)?.Length ?? 0} results.");
+    }
 
     return result;
   }
@@ -60,14 +51,13 @@ public class Dispatcher
   {
     return await Execute(
       () => ExecuteCommand(command),
-      $"Command {command.GetType().Name}",
       command
     );
   }
 
   private async Task<CommandResult> ExecuteCommand<TCommand>(TCommand command) where TCommand : ICommand
   {
-    var commandExecutor = _serviceProvider.GetService<ICommandExecutor<TCommand>>();
+    var commandExecutor = serviceProvider.GetService<ICommandExecutor<TCommand>>();
     if (commandExecutor == null)
     {
       throw new Exception($"No command executor registered for command of type {command.GetType()}");
@@ -83,27 +73,38 @@ public class Dispatcher
   private void InvalidateCache(CommandResult commandResult)
   {
     string[] affectedUserIds = commandResult.AffectedUserIds
-      .Union(new[] { _repository.CurrentUser.Value.Id! })
+      .Union(new[] { repository.CurrentUser.Value.Id! })
       .ToArray();
 
-    _queryCache.Invalidate(affectedUserIds);
+    queryCache.Invalidate(affectedUserIds);
   }
 
   private async Task<TExecutionResult> Execute<TExecutionResult>(
     Func<Task<TExecutionResult>> action,
-    string labelPrefix,
     object payload
   )
   {
-    using (_logger!.BeginScope(new Dictionary<string, object> { ["Payload"] = JsonConvert.SerializeObject(payload) }))
+    string name = payload.GetType().Name;
+
+    using (logger.BeginScope(
+             new Dictionary<string, object>
+             {
+               ["ActionKind"] = payload is IQuery ? "query" : "command",
+               ["PayloadType"] = name,
+               ["Payload"] = JsonConvert.SerializeObject(payload),
+             }
+           ))
     {
-      _logger?.LogInformation($"{labelPrefix}: Start");
+      logger.LogInformation($"{name}: Starting");
 
       var watch = Stopwatch.StartNew();
-
       TExecutionResult result = await action();
+      watch.Stop();
 
-      _logger?.LogInformation($"{labelPrefix}: Executed in {watch.ElapsedMilliseconds}ms");
+      using (logger.BeginScope(new Dictionary<string, object> { ["Duration"] = watch.ElapsedMilliseconds }))
+      {
+        logger.LogInformation($"{name}: Completed in {watch.ElapsedMilliseconds}ms");
+      }
 
       return result;
     }
