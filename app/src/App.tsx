fff3b9f2a0b8f1348902ac947ae61d3c048eebb2
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-03-13 08:29:46 +0100

    Enable oneSignal manually

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,22 +6,17 @@ import { AppHost } from "./AppHost";
 import { PageContextProvider } from "./components/layout/pages/PageContextProvider";
 import { DialogContextProvider } from "./components/layout/dialogs/DialogContextProvider";
 import { AppContextProvider } from "./AppContextProvider";
-import { useOneSignal } from "./pwa/useOneSignal";
 
-export const App: React.FC<{ user: IUser }> = ({ user }) => {
-  useOneSignal(user.globalUniqueId);
-
-  return (
-    <AppContextProvider user={user}>
-      <ReactQueryProviderWrapper>
-        <BrowserRouter>
-          <PageContextProvider>
-            <DialogContextProvider>
-              <AppHost />
-            </DialogContextProvider>
-          </PageContextProvider>
-        </BrowserRouter>
-      </ReactQueryProviderWrapper>
-    </AppContextProvider>
-  );
-};
+export const App: React.FC<{ user: IUser }> = ({ user }) => (
+  <AppContextProvider user={user}>
+    <ReactQueryProviderWrapper>
+      <BrowserRouter>
+        <PageContextProvider>
+          <DialogContextProvider>
+            <AppHost />
+          </DialogContextProvider>
+        </PageContextProvider>
+      </BrowserRouter>
+    </ReactQueryProviderWrapper>
+  </AppContextProvider>
+);
--- app/src/pwa/PwaSettingsPage.tsx
+++ app/src/pwa/PwaSettingsPage.tsx
@@ -2,11 +2,26 @@ import { Page } from "../components/layout/pages/Page";
 import { PageSection } from "../components/layout/pages/PageSection";
 import { Button } from "@mui/material";
 import { ServerApi } from "../serverApi/ServerApi";
+import { setUpOnSignal } from "./useOneSignal";
+import { useAppContext } from "../AppContext";
 
 export const PwaSettingsPage: React.FC = () => {
+  const { user } = useAppContext();
+
   return (
     <Page pageType="settings" title="PWA Settings" documentTitle="PWA Settings">
       <PageSection title="Notifications">
+        <p>
+          <Button
+            variant={"outlined"}
+            onClick={() => {
+              setUpOnSignal(user.globalUniqueId);
+            }}
+          >
+            Enable notifications
+          </Button>
+        </p>
+
         <p>
           <Button
             variant={"outlined"}
