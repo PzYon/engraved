e826e38539f95f588ae50b159e4f64009ff20d19
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-03-13 23:09:18 +0100

    Basic implementation of job

--- api/Engraved.Api/Source/Jobs/ScheduleJob.cs
+++ api/Engraved.Api/Source/Jobs/ScheduleJob.cs
@@ -1,6 +1,13 @@
-﻿namespace Engraved.Api.Jobs;
+﻿using Engraved.Core.Application;
+using Engraved.Core.Application.Persistence;
+using Engraved.Core.Application.Queries.Search.Entities;
+using Engraved.Core.Domain.Entries;
+using Engraved.Core.Domain.Journals;
 
-public class ScheduleJob(ILogger<ScheduleJob> logger) : BackgroundService
+namespace Engraved.Api.Jobs;
+
+public class ScheduleJob(ILogger<ScheduleJob> logger, IRepository repository, IDateService dateService)
+  : BackgroundService
 {
   private readonly TimeSpan _period = TimeSpan.FromSeconds(30);
 
@@ -13,8 +20,51 @@ public class ScheduleJob(ILogger<ScheduleJob> logger) : BackgroundService
         && await timer.WaitForNextTickAsync(stoppingToken)
       )
       {
-        logger.LogInformation("I am job.");
+        Log("Start");
+
+        IEntry[] entries = await repository.GetLastEditedEntries(
+          Array.Empty<string>(),
+          null,
+          Array.Empty<JournalType>(),
+          null,
+          true
+        );
+
+        foreach (IEntry entry in entries)
+        {
+          if (entry.Schedule?.NextOccurrence < dateService.UtcNow)
+          {
+            Log(
+              $"Would send notification for entry with ID {entry.Id}, scheduled at {entry.Schedule.NextOccurrence}"
+            );
+          }
+        }
+
+        IJournal[] journals = await repository.GetAllJournals(
+          null,
+          Array.Empty<JournalType>(),
+          Array.Empty<string>(),
+          null,
+          true
+        );
+
+        foreach (IJournal journal in journals)
+        {
+          if (journal.Schedule?.NextOccurrence < dateService.UtcNow)
+          {
+            Log(
+              $"Would send notification for journal with ID {journal.Id}, scheduled at {journal.Schedule.NextOccurrence}"
+            );
+          }
+        }
+
+        Log("End");
       }
     }
+  }
+
+  private void Log(string message)
+  {
+    logger.LogInformation("[Background-Job]: " + message);
   }
 }
