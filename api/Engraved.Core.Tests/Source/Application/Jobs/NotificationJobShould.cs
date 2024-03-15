21fbff52f3dd791bccfaf13f94ca135af192d967
Markus Doggweiler <PzYon@users.noreply.github.com>
2024-03-15 13:50:00 +0100

    Schedule job boilerplate (#1812)

---
+++ api/Engraved.Core.Tests/Source/Application/Jobs/NotificationJobShould.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Engraved.Core.Application.Persistence.Demo;
+using Engraved.Core.Domain.Notifications;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+
+namespace Engraved.Core.Application.Jobs;
+
+public class TestNotificationService : INotificationService
+{
+  public Task SendNotification(Notification notification)
+  {
+    throw new System.NotImplementedException();
+  }
+}
+
+public class NotificationJobShould
+{
+  [Test]
+  public async Task Initial()
+  {
+    var repository = new InMemoryRepository();
+    var dateService = new FakeDateService();
+    var notificationService = new TestNotificationService();
+
+    var job = new NotificationJob(
+      NullLogger<NotificationJob>.Instance,
+      repository,
+      notificationService,
+      dateService
+    );
+
+    await job.Execute();
+  }
+}
