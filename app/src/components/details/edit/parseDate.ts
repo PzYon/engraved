47dcebfce67393478d691c053e449ada5a3e4b1d
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-03-23 21:24:29 +0100

    Improve date parsing

--- app/src/components/details/edit/parseDate.ts
+++ app/src/components/details/edit/parseDate.ts
@@ -6,16 +6,22 @@ export interface IParsedDate {
 }
 
 export const parseDate = (value: string, referenceDate?: Date): IParsedDate => {
-  const parsed = chrono.parse(value, referenceDate, { forwardDate: true });
+  const preparedValue = value
+    .replace(/(\d\d)(\d\d)/, "$1:$2")
+    .replace("tom", "tomorrow");
+
+  const parsed = chrono.parse(preparedValue, referenceDate, {
+    forwardDate: true,
+  });
 
   if (parsed[0].date) {
     return {
       date: parsed[0].date(),
-      text: value.replace(parsed[0].text, "").trim(),
+      text: preparedValue.replace(parsed[0].text, "").replace("  ", " ").trim(),
     };
   }
 
   return {
-    text: value,
+    text: preparedValue,
   };
 };
