638e14497e28c1da3ce7aadb37108542d9ab6f08
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-03-27 10:11:21 +0100

    Make sure multiple notifications can be displayed

--- api/Engraved.Notifications.OneSignal/Source/OneSignalNotificationService.cs
+++ api/Engraved.Notifications.OneSignal/Source/OneSignalNotificationService.cs
@@ -56,10 +56,8 @@ public class OneSignalNotificationService(IOptions<OneSignalConfig> config) : IN
         )
         .OfType<Button>()
         .ToList(),
-      // the following two properties are somehow required in order to show multiple
-      // notifications at the same time.
-      webPushTopic: config.Value.AppId,
-      id: Guid.NewGuid().ToString()
+      // the following property is required to show multiple notifications at the same time.
+      webPushTopic: Guid.NewGuid().ToString()
     );
 
     if (doNotSend)
