68940e9a558a5120ad3676dc7f538c67d5307d97
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-04-02 11:12:10 +0200

    Improve OnNotification dialog

--- app/src/components/details/OnNotification.tsx
+++ app/src/components/details/OnNotification.tsx
@@ -4,27 +4,35 @@ import { IJournal } from "../../serverApi/IJournal";
 import { IEntry } from "../../serverApi/IEntry";
 import { ActionIconButtonGroup } from "../common/actions/ActionIconButtonGroup";
 import { ActionFactory } from "../common/actions/ActionFactory";
+import { Scrap } from "./scraps/Scrap";
+import { IScrapEntry } from "../../serverApi/IScrapEntry";
 
 export const OnNotification: React.FC<{
   journal: IJournal;
   entry?: IEntry;
   onCancel: () => void;
 }> = ({ journal, entry }) => {
+  if (entry) {
+    return (
+      <Scrap
+        scrap={entry as IScrapEntry}
+        journalName={journal.name}
+        hasFocus={true}
+        propsRenderStyle={"all"}
+        withoutSection={true}
+      />
+    );
+  }
+
   return (
-    <div>
-      <ActionsContainer>
-        <ActionIconButtonGroup
-          actions={[
-            entry
-              ? ActionFactory.deleteEntry(entry)
-              : ActionFactory.deleteJournal(journal.id, false),
-            entry
-              ? ActionFactory.editEntrySchedule(journal.id, entry.id)
-              : ActionFactory.editJournalSchedule(journal.id, false),
-          ]}
-        />
-      </ActionsContainer>
-    </div>
+    <ActionsContainer>
+      <ActionIconButtonGroup
+        actions={[
+          ActionFactory.deleteJournal(journal.id, false),
+          ActionFactory.editJournalSchedule(journal.id, false),
+        ]}
+      />
+    </ActionsContainer>
   );
 };
 
