9ec7f520701de7c93f1be339ab5b4a827565f66d
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-04-08 09:13:39 +0200

    Fix ReadonlyTitleRow.tsx

--- app/src/components/overview/ReadonlyTitleRow.tsx
+++ app/src/components/overview/ReadonlyTitleRow.tsx
@@ -2,6 +2,7 @@ import { ISchedule } from "../../serverApi/ISchedule";
 import { useAppContext } from "../../AppContext";
 import { Properties } from "../common/Properties";
 import { getScheduleProperty } from "./scheduled/scheduleUtils";
+import { useDisplayModeContext } from "./overviewList/DisplayModeContext";
 
 export const ReadonlyTitleRow: React.FC<{
   title: React.ReactNode;
@@ -9,17 +10,20 @@ export const ReadonlyTitleRow: React.FC<{
   schedules: Record<string, ISchedule>;
 }> = ({ title, hasFocus, schedules }) => {
   const { user } = useAppContext();
+  const { isCompact } = useDisplayModeContext();
+
+  console.log("has focus: ", hasFocus);
 
   return (
     <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
       <span style={{ flexGrow: 1 }}>{title}</span>
-      {hasFocus ? null : (
+      {!hasFocus && isCompact ? (
         <Properties
           properties={[
             getScheduleProperty(schedules?.[user.id]?.nextOccurrence),
           ]}
         />
-      )}
+      ) : null}
     </div>
   );
 };
