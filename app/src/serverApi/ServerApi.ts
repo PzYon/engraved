1dc2d08cc63d5ba7c2a99cb2cc326ac7d4ceb9bf
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-05-15 22:27:09 +0200

    GetEntryQuery

--- app/src/serverApi/ServerApi.ts
+++ app/src/serverApi/ServerApi.ts
@@ -156,7 +156,9 @@ export class ServerApi {
   }
 
   static async getActiveEntry(journalId: string): Promise<IEntry> {
-    return await ServerApi.executeRequest(`/entries/${journalId}/active`);
+    return await ServerApi.executeRequest(
+      `/entries/journal/${journalId}/active`,
+    );
   }
 
   static async getAllEntries(
@@ -299,7 +301,9 @@ export class ServerApi {
 
     const params = urlParams.length ? `?${urlParams.join("&")}` : "";
 
-    return await ServerApi.executeRequest(`/entries/${journalId}${params}`);
+    return await ServerApi.executeRequest(
+      `/entries/journal/${journalId}${params}`,
+    );
   }
 
   static async upsertEntry(
