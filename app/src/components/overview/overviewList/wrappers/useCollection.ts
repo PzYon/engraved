8c56014c61b4337f7d68f5476a95008cf0ce39fb
Markus Doggweiler <PzYon@users.noreply.github.com>
2024-05-30 01:02:32 +0200

    WrapperCollection: Focus improvements (#2060)

--- app/src/components/overview/overviewList/wrappers/useCollection.ts
+++ app/src/components/overview/overviewList/wrappers/useCollection.ts
@@ -1,16 +1,21 @@
 import { useHotkeys } from "react-hotkeys-hook";
 import { WrapperCollection } from "./WrapperCollection";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { WrapperCollectionItem } from "./WrapperCollectionItem";
 
 export function useCollection(deps: unknown[]) {
   const [focusIndex, setFocusIndex] = useState(-1);
+  const [itemId, setItemId] = useState<string>(null);
 
   const collection = useMemo(
-    () => new WrapperCollection(focusIndex, setFocusIndex),
+    () => new WrapperCollection(focusIndex, setFocusIndex, setItemId),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [...deps],
   );
+
+  useEffect(() => {
+    collection.setFocusForId(itemId);
+  }, [collection]);
 
   useHotkeys("alt+up", () => {
     collection.moveFocusUp();
@@ -22,7 +27,6 @@ export function useCollection(deps: unknown[]) {
 
   return {
     collection,
-    focusIndex,
     addItem: (wrapper: WrapperCollectionItem) => collection.add(wrapper),
   };
 }
