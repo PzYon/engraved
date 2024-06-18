b1f4d707175468188c13fd6c3bd890b97e968d9f
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-06-19 01:01:47 +0200

    Fix NotificationDoneAction

--- app/src/components/overview/scheduled/ScheduledInfo.tsx
+++ app/src/components/overview/scheduled/ScheduledInfo.tsx
@@ -3,10 +3,14 @@ import { DateFormat } from "../../common/dateTypes";
 import { ReplayOutlined } from "@mui/icons-material";
 import { ISchedule } from "../../../serverApi/ISchedule";
 import React from "react";
+import { parseDate } from "../../details/edit/parseDate";
+import { isAfter } from "date-fns";
 
-export const ScheduledInfo: React.FC<{ schedule: ISchedule }> = ({
-  schedule,
-}) => {
+export const ScheduledInfo: React.FC<{
+  schedule: ISchedule;
+  showNextIfPassed?: boolean;
+  showRecurrenceInfo?: boolean;
+}> = ({ schedule, showNextIfPassed, showRecurrenceInfo }) => {
   return (
     <span
       style={{
@@ -15,9 +19,14 @@ export const ScheduledInfo: React.FC<{ schedule: ISchedule }> = ({
       }}
     >
       <FormatDate
-        value={schedule?.nextOccurrence}
+        value={getDateValue()}
         dateFormat={DateFormat.relativeToNow}
       />
+      {schedule.recurrence?.dateString && showRecurrenceInfo ? (
+        <span style={{ marginLeft: "8px" }}>
+          (&quot;{schedule.recurrence.dateString}&quot;)
+        </span>
+      ) : null}
       {schedule.recurrence?.dateString ? (
         <span
           title={schedule.recurrence.dateString}
@@ -28,4 +37,17 @@ export const ScheduledInfo: React.FC<{ schedule: ISchedule }> = ({
       ) : null}
     </span>
   );
+
+  function getDateValue() {
+    if (
+      showNextIfPassed &&
+      schedule.recurrence?.dateString &&
+      schedule.nextOccurrence &&
+      isAfter(new Date(), schedule.nextOccurrence)
+    ) {
+      return parseDate(schedule.recurrence.dateString).date.toString();
+    }
+
+    return schedule.nextOccurrence;
+  }
 };
