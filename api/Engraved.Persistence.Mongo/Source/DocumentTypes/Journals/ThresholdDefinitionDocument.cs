71b1e6a6374567e887182ea2f3ea5d9f6c499178
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-08-17 23:42:26 +0200

    ThresholdDefinitionDocument

---
+++ api/Engraved.Persistence.Mongo/Source/DocumentTypes/Journals/ThresholdDefinitionDocument.cs
@@ -0,0 +1,10 @@
+﻿using Engraved.Core.Domain.Journals;
+
+namespace Engraved.Persistence.Mongo.DocumentTypes.Journals;
+
+public class ThresholdDefinitionDocument
+{
+  public double Value { get; set; }
+  
+  public ThresholdScope Scope {get; set;}
+}
