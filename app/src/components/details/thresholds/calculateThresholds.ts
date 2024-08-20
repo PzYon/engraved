aa32b8f08ca3cb5b886aa0c33b725d1bc2957d6e
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-08-21 00:23:07 +0200

    Cleanup

--- app/src/components/details/thresholds/calculateThresholds.ts
+++ app/src/components/details/thresholds/calculateThresholds.ts
@@ -3,6 +3,7 @@ import { IJournalThresholds } from "../../../serverApi/IJournalThresholds";
 import { IEntry } from "../../../serverApi/IEntry";
 import { IThresholdValues } from "../../../serverApi/IThresholdValues";
 import { JournalTypeFactory } from "../../../journalTypes/JournalTypeFactory";
+import { IJournalType } from "../../../journalTypes/IJournalType";
 
 export const calculateThresholds = (
   journalType: JournalType,
@@ -14,44 +15,64 @@ export const calculateThresholds = (
   const type = JournalTypeFactory.create(journalType);
 
   for (const attributeKey of Object.keys(thresholds ?? {})) {
-    const attributeValueKeys = Object.keys(thresholds[attributeKey]);
-
-    for (const attributeValueKey of attributeValueKeys) {
+    for (const attributeValueKey of Object.keys(thresholds[attributeKey])) {
       const definition = thresholds[attributeKey][attributeValueKey];
 
-      if (attributeKey === "-") {
-        thresholdValues["-"] = {
-          "-": {
-            thresholdDefinition: definition,
-            actualValue: entries.reduce(
-              (previousValue, currentValue) =>
-                previousValue + type.getValue(currentValue),
-              0,
-            ),
-          },
-        };
-      } else {
-        if (!thresholdValues[attributeKey]) {
-          thresholdValues[attributeKey] = {};
-        }
-
-        thresholdValues[attributeKey][attributeValueKey] = {
-          thresholdDefinition: definition,
-          actualValue: entries
-            .filter((e) =>
-              e.journalAttributeValues?.[attributeKey]?.includes(
-                attributeValueKey,
-              ),
-            )
-            .reduce(
-              (previousValue, currentValue) =>
-                previousValue + type.getValue(currentValue),
-              0,
-            ),
-        };
+      if (!thresholdValues[attributeKey]) {
+        thresholdValues[attributeKey] = {};
       }
+
+      thresholdValues[attributeKey][attributeValueKey] = {
+        thresholdDefinition: definition,
+        actualValue: getActualValue(
+          attributeKey,
+          type,
+          entries,
+          attributeValueKey,
+        ),
+      };
     }
   }
 
   return thresholdValues;
 };
+
+function getActualValue(
+  attributeKey: string,
+  type: IJournalType,
+  entries: IEntry[],
+  attributeValueKey: string,
+) {
+  if (attributeKey === "-") {
+    return getSumUnfiltered(type, entries);
+  }
+
+  return getSumFilteredByAttributeValue(
+    type,
+    entries,
+    attributeKey,
+    attributeValueKey,
+  );
+}
+
+function getSumFilteredByAttributeValue(
+  type: IJournalType,
+  entries: IEntry[],
+  attributeKey: string,
+  attributeValueKey: string,
+) {
+  return getSum(
+    type,
+    entries.filter((e) =>
+      e.journalAttributeValues?.[attributeKey]?.includes(attributeValueKey),
+    ),
+  );
+}
+
+function getSumUnfiltered(type: IJournalType, entries: IEntry[]) {
+  return getSum(type, entries);
+}
+
+function getSum(type: IJournalType, entries: IEntry[]) {
+  return entries.reduce((total, entry) => total + type.getValue(entry), 0);
+}
