1042cbe9e4975119135383ba32e90b9264eae0d2
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-08-28 21:56:33 +0200

    getSortedEntries

--- app/src/components/details/entriesTable/EntriesAgenda.tsx
+++ app/src/components/details/entriesTable/EntriesAgenda.tsx
@@ -8,6 +8,38 @@ import { HistoryToggleOff } from "@mui/icons-material";
 import { paperBorderRadius } from "../../../theming/engravedTheme";
 import { FormatDate } from "../../common/FormatDate";
 
+interface ISortedEntries {
+  entries: IEntry[];
+  gaps: {
+    label: string;
+  }[];
+}
+
+function getSortedEntries(entries: IEntry[]): ISortedEntries {
+  const result: ISortedEntries = {
+    entries: entries.sort((a, b) => {
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      return (
+        ((new Date(a.dateTime) as any) - (new Date(b.dateTime) as any)) * -1
+      );
+    }),
+    gaps: [],
+  };
+
+  for (let i = 0; i < result.entries.length; i++) {
+    if (i < result.entries.length - 1) {
+      result.gaps.push({
+        label: formatDistance(
+          entries[i + 1].dateTime,
+          result.entries[i].dateTime,
+        ),
+      });
+    }
+  }
+
+  return result;
+}
+
 export const EntriesAgenda: React.FC<{
   journal: IJournal;
   entries: IEntry[];
@@ -15,60 +47,54 @@ export const EntriesAgenda: React.FC<{
   if (!journal) {
     return null;
   }
-
   const journalType = JournalTypeFactory.create(journal.type);
+
+  const sortedEntries = getSortedEntries(entries);
 
   return (
     <div style={{ marginTop: "24px", marginBottom: "24px" }}>
-      {entries
-        .sort((a, b) => {
-          /* eslint-disable @typescript-eslint/no-explicit-any */
-          return (
-            ((new Date(a.dateTime) as any) - (new Date(b.dateTime) as any)) * -1
-          );
-        })
-        .map((entry, index) => {
-          return (
-            <>
-              <Paper
-                key={entry.id}
-                sx={{ p: 2, borderRadius: paperBorderRadius }}
-              >
-                <Typography>
-                  <Chip
-                    label={journalType.getValue(entry).toString()}
-                    sx={{
-                      backgroundColor: "primary.main",
-                      color: "common.white",
-                      fontSize: "small",
-                      height: "22px",
-                      mr: 2,
-                    }}
-                  ></Chip>
-                  <FormatDate value={entry.dateTime} />
-                </Typography>
-              </Paper>
-              {index < entries.length - 1 ? (
-                <Typography
+      {sortedEntries.entries.map((entry, index) => {
+        return (
+          <>
+            <Paper
+              key={entry.id}
+              sx={{ p: 2, borderRadius: paperBorderRadius }}
+            >
+              <Typography>
+                <Chip
+                  label={journalType.getValue(entry).toString()}
                   sx={{
-                    pt: 1,
-                    pb: 1,
-                    pl: 1,
-                    ml: 3,
-                    fontSize: "smaller",
-                    display: "flex",
-                    alignItems: "center",
-                    borderLeft: "3px solid white",
-                    color: "primary.main",
+                    backgroundColor: "primary.main",
+                    color: "common.white",
+                    fontSize: "small",
+                    height: "22px",
+                    mr: 2,
                   }}
-                >
-                  <HistoryToggleOff fontSize="small" sx={{ mr: 1 }} />
-                  {formatDistance(entries[index + 1].dateTime, entry.dateTime)}
-                </Typography>
-              ) : null}
-            </>
-          );
-        })}
+                ></Chip>
+                <FormatDate value={entry.dateTime} />
+              </Typography>
+            </Paper>
+            {sortedEntries.gaps[index] ? (
+              <Typography
+                sx={{
+                  pt: 1,
+                  pb: 1,
+                  pl: 1,
+                  ml: 3,
+                  fontSize: "smaller",
+                  display: "flex",
+                  alignItems: "center",
+                  borderLeft: "3px solid white",
+                  color: "primary.main",
+                }}
+              >
+                <HistoryToggleOff fontSize="small" sx={{ mr: 1 }} />
+                {sortedEntries.gaps[index].label}
+              </Typography>
+            ) : null}
+          </>
+        );
+      })}
     </div>
   );
 };
