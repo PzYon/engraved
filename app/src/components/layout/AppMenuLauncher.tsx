ff3201de75e45ccd65d4e14b9a3ff2779b403e7c
Markus Doggweiler <PzYon@users.noreply.github.com>
2024-10-07 00:34:38 +0200

    App Menu (first [very primitive] version) (#2392)

---
+++ app/src/components/layout/AppMenuLauncher.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { AppMenu } from "./AppMenu";
+import { Drawer, styled } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+export const AppMenuLauncher: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <StyledDrawer open={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+        <AppMenu close={() => setIsMenuOpen(false)} />
+      </StyledDrawer>
+      <StyledMenuIcon
+        sx={{
+          color: "white",
+          mr: 2,
+          cursor: "pointer",
+        }}
+        fontSize="large"
+        onClick={() => setIsMenuOpen(true)}
+      />
+    </>
+  );
+};
+
+const StyledMenuIcon = styled(Menu)`
+  @media screen and (min-width: 1265px) {
+    position: fixed;
+    top: 15px;
+    left: 10px;
+  }
+`;
+
+const StyledDrawer = styled(Drawer)`
+  .MuiPaper-root {
+    border-radius: 0;
+  }
+`;
