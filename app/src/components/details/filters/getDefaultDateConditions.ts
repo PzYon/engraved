fc83d3e38a5f51f6b5cb4c881d7a36bcda336e91
Markus Doggweiler <markus.doggweiler@gmail.com>
2024-11-03 00:14:23 +0100

    Fix bug where date filter was wrongfully reset

--- app/src/components/details/filters/getDefaultDateConditions.ts
+++
@@ -1,6 +0,0 @@
-import { createDateConditions } from "./createDateConditions";
-import { journalDefaultUiSettings } from "../journalDefaultUiSettings";
-
-export const getDefaultDateConditions = () => {
-  return createDateConditions(journalDefaultUiSettings.dateFilter, new Date());
-};
