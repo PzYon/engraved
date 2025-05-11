d8a502a91366962dd7c39d8e0396483f42bbde19
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-05-11 21:24:11 +0200

    Improve FunkyList

--- app/src/components/common/funkyList/FunkyList.tsx
+++ app/src/components/common/funkyList/FunkyList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const items = [
   {
     id: "a",
@@ -13,16 +15,39 @@ const items = [
   },
 ];
 
+function giveFocusToElement(id: string) {
+  document.getElementById("funky-list-" + id).focus();
+}
+
 export const FunkyList: React.FC = () => {
+  const [activeItemId, setActiveItemId] = useState<string>(undefined);
+
+  useEffect(() => {
+    console.log("active item id", activeItemId);
+  }, [activeItemId]);
+
   return (
     <div>
       {items.map((item, index) => {
+        const isFirst = index === 0;
+        const isLast = index === items.length - 1;
+
         return (
           <div
+            id={"funky-list-" + item.id}
             key={item.id}
             tabIndex={index}
-            onFocusCapture={(e) => console.log("captured focus", item.id, e)}
-            onFocus={() => console.log("on focus", item.id)}
+            onFocus={() => {
+              console.log("on focus", item.id);
+              setActiveItemId(item.id);
+            }}
+            onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+              if (e.key === "ArrowDown" && isLast) {
+                giveFocusToElement(items[0].id);
+              } else if (e.key === "ArrowUp" && isFirst) {
+                giveFocusToElement(items[items.length - 1].id);
+              }
+            }}
           >
             {item.label}
           </div>
