e87b1a718d247c26f35c2ad92693a7ebe1378fa9
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-05-11 09:52:36 +0200

    Trying a new list...

--- app/src/components/common/funkyList/FunkyList.tsx
+++ app/src/components/common/funkyList/FunkyList.tsx
@@ -1,3 +1,33 @@
+const items = [
+  {
+    id: "a",
+    label: "Foo",
+  },
+  {
+    id: "b",
+    label: "Bar",
+  },
+  {
+    id: "c",
+    label: "Baz",
+  },
+];
+
 export const FunkyList: React.FC = () => {
-  return <div>TEST</div>;
+  return (
+    <div>
+      {items.map((item, index) => {
+        return (
+          <div
+            key={item.id}
+            tabIndex={index}
+            onFocusCapture={(e) => console.log("captured focus", item.id, e)}
+            onFocus={() => console.log("on focus", item.id)}
+          >
+            {item.label}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
