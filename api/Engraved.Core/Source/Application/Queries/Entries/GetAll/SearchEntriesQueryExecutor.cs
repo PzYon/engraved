2e88424dcd4898b75845ccf50d90878da166b5ec
Markus Doggweiler <PzYon@users.noreply.github.com>
2025-06-06 00:14:11 +0200

    Fix old schedules not being shown (#2486)

--- api/Engraved.Core/Source/Application/Queries/Entries/GetAll/SearchEntriesQueryExecutor.cs
+++ api/Engraved.Core/Source/Application/Queries/Entries/GetAll/SearchEntriesQueryExecutor.cs
@@ -12,19 +12,19 @@ public class SearchEntriesQueryExecutor(IUserScopedRepository repository)
   public async Task<SearchEntriesQueryResult> Execute(SearchEntriesQuery query)
   {
     var allJournals = await repository.GetAllJournals(null, null, null, null, 100);
-    string[] allJournalIds = allJournals.Select(j => j.Id!).ToArray();
+    var allJournalIds = allJournals.Select(j => j.Id!).ToArray();
 
-    IEntry[] allEntries = await repository.SearchEntries(
+    var allEntries = await repository.SearchEntries(
       query.SearchText,
       query.ScheduledOnly ? ScheduleMode.CurrentUserOnly : ScheduleMode.None,
       query.JournalTypes,
       allJournalIds,
-      query.Limit ?? 20,
+      query.Limit ?? 100,
       repository.CurrentUser.Value.Id,
       query.OnlyConsiderTitle.HasValue && query.OnlyConsiderTitle.Value
     );
 
-    string[] relevantJournalIds = allEntries.Select(e => e.ParentId).ToArray();
+    var relevantJournalIds = allEntries.Select(e => e.ParentId).ToArray();
 
     return new SearchEntriesQueryResult
     {
