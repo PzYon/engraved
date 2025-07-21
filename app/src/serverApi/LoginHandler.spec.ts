07b71de6cc836d903d1a12127914f72326b1fec2
Markus Doggweiler <PzYon@users.noreply.github.com>
2025-07-22 00:34:57 +0200

    Fix skipped tests (#2500)

--- app/src/serverApi/LoginHandler.spec.ts
+++ app/src/serverApi/LoginHandler.spec.ts
@@ -1,77 +1,74 @@
 import { LoginHandler } from "./LoginHandler";
 
-function getPromise<T>(delayMs: number, returnValue: T) {
+describe("LoginHandler", () => {
+  describe("loginAndRetry", () => {
+    it("should execute all methods once logged in", async () => {
+      let loginCount = 0;
+      const results: string[] = [];
+
+      const loginHandler = new LoginHandler(() => {
+        loginCount++;
+        return delayAndReturn(1000, undefined);
+      });
+
+      loginHandler.loginAndRetry(() =>
+        delayAndReturn(100, "first").then((x) => results.push(x)),
+      );
+
+      loginHandler.loginAndRetry(() =>
+        delayAndReturn(200, "second").then((x) => results.push(x)),
+      );
+
+      loginHandler.loginAndRetry(() =>
+        delayAndReturn(300, "third").then((x) => results.push(x)),
+      );
+
+      await delayAndReturn(2000);
+
+      expect(loginCount).toBe(1);
+
+      expect(results.length).toBe(3);
+      expect(results[0]).toBe("first");
+      expect(results[1]).toBe("second");
+      expect(results[2]).toBe("third");
+    });
+
+    it("should clear 'functions to call' after login", async () => {
+      let loginCount = 0;
+      const results: string[] = [];
+
+      const loginHandler = new LoginHandler(() => {
+        loginCount++;
+        return delayAndReturn(500, undefined);
+      });
+
+      loginHandler.loginAndRetry(() =>
+        delayAndReturn(100, "first").then((x) => results.push(x)),
+      );
+
+      await delayAndReturn(1000);
+
+      expect(loginCount).toBe(1);
+      expect(results.length).toBe(1);
+      expect(results[0]).toBe("first");
+
+      loginHandler.loginAndRetry(() =>
+        delayAndReturn(100, "second").then((x) => results.push(x)),
+      );
+
+      await delayAndReturn(1000);
+
+      expect(loginCount).toBe(2);
+      expect(results.length).toBe(2);
+      expect(results[1]).toBe("second");
+    });
+  });
+});
+
+function delayAndReturn<T>(delayMs: number, returnValue?: T) {
   return new Promise<T>((resolve) => {
     setTimeout(() => {
       resolve(returnValue);
     }, delayMs);
   });
 }
-
-describe("LoginHandler", () => {
-  describe("loginAndRetry", () => {
-    it.skip("should execute all methods once logged in", () => {
-      let loginCount = 0;
-      const results: string[] = [];
-
-      const loginHandler = new LoginHandler(() => {
-        loginCount++;
-        return getPromise(1000, undefined);
-      });
-
-      loginHandler.loginAndRetry(() =>
-        getPromise(100, "first").then((x) => results.push(x)),
-      );
-
-      loginHandler.loginAndRetry(() =>
-        getPromise(200, "second").then((x) => results.push(x)),
-      );
-
-      loginHandler.loginAndRetry(() =>
-        getPromise(300, "third").then((x) => results.push(x)),
-      );
-
-      setTimeout(() => {
-        expect(loginCount).toBe(1);
-
-        expect(results.length).toBe(3);
-        expect(results[0]).toBe("first");
-        expect(results[1]).toBe("second");
-        expect(results[2]).toBe("third");
-
-        // done();
-      }, 2000);
-    });
-
-    it.skip("should clear 'functions to call' after login", () => {
-      let loginCount = 0;
-      const results: string[] = [];
-
-      const loginHandler = new LoginHandler(() => {
-        loginCount++;
-        return getPromise(500, undefined);
-      });
-
-      loginHandler.loginAndRetry(() =>
-        getPromise(100, "first").then((x) => results.push(x)),
-      );
-
-      setTimeout(() => {
-        expect(loginCount).toBe(1);
-        expect(results.length).toBe(1);
-        expect(results[0]).toBe("first");
-
-        loginHandler.loginAndRetry(() =>
-          getPromise(100, "second").then((x) => results.push(x)),
-        );
-
-        setTimeout(() => {
-          expect(loginCount).toBe(2);
-          expect(results.length).toBe(2);
-          expect(results[1]).toBe("second");
-          // done();
-        }, 1000);
-      }, 1000);
-    });
-  });
-});
