603ee40d7a3421c82841aa4965651eac94c4dbb0
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-08-03 21:19:59 +0200

    ???

--- tests/src/poms/scrapsJournalPage.ts
+++ tests/src/poms/scrapsJournalPage.ts
@@ -11,16 +11,20 @@ export class ScrapsJournalPage extends JournalPage {
     await this.clickPageAction("Add entry");
 
     await expect(this.page.getByTestId("add-new-scrap")).toBeVisible();
-    await this.page.waitForTimeout(1000);
 
     if (
       (await this.page
-        .getByTestId("add-new-scrap")
+        //.getByTestId("add-new-scrap")
         .locator("[data-scrap-type='Markdown']")
         .count()) === 0
     ) {
       await this.page
         .getByRole("button", { name: "Change type to list" })
+        .click();
+
+      await this.page
+        .getByLabel("Change type to list")
+        //.getByRole("button", { name: "Change type to list" })
         .click();
 
       await this.page.waitForTimeout(1000);
