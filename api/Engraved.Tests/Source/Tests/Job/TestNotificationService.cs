9de969b58153658fec8f13a27a5c771826f8cd62
Markus Doggweiler <PzYon@users.noreply.github.com>
2025-08-20 00:42:13 +0200

    Cancel not (#2536)

--- api/Engraved.Tests/Source/Tests/Job/TestNotificationService.cs
+++ api/Engraved.Tests/Source/Tests/Job/TestNotificationService.cs
@@ -4,8 +4,10 @@ namespace Engraved.Tests.Tests.Job;
 
 public class TestNotificationService : INotificationService
 {
-  public Task SendNotification(ClientNotification notification, bool doNotSend)
+  public void CancelNotification(string notificationId) { }
+
+  public Task<string?> SendNotification(ClientNotification notification, bool doNotSend)
   {
-    return Task.CompletedTask;
+    return Task.FromResult<string?>(Guid.NewGuid().ToString());
   }
 }
