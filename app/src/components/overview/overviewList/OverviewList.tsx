76298b8fd42358029c2b3ee0bcdb9d325f20e950
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-08-26 23:38:17 +0200

    Cleanup again

--- app/src/components/overview/overviewList/OverviewList.tsx
+++ app/src/components/overview/overviewList/OverviewList.tsx
@@ -53,8 +53,6 @@ const OverviewListInternal: React.FC<IOverviewListProps> = ({
 
   const [inMemorySearchText, setInMemorySearchText] = useState("");
 
-  console.log("in memory search text", inMemorySearchText);
-
   useEffect(() => {
     if (activeItemIdFromUrl && activeItemId !== activeItemIdFromUrl) {
       setActiveItemId(activeItemIdFromUrl);
@@ -81,11 +79,12 @@ const OverviewListInternal: React.FC<IOverviewListProps> = ({
         if (onKeyDown) {
           onKeyDown?.(e);
         } else {
-          if (e.key.match(/\w/)) {
-            console.log("JOp", e.key);
+          if (e.key.match(/^\w$/)) {
             setInMemorySearchText((current) => current + e.key);
           } else if (e.key === "Backspace") {
-            setInMemorySearchText((current) => current.slice(0, -1));
+            setInMemorySearchText((current) =>
+              current.substring(0, current.length - 1),
+            );
           }
         }
       }
