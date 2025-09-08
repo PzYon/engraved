457eb2d548584b316ac25808640e9de74f2b2dcf
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-09-08 13:45:50 +0200

    PR review

--- app/src/pwa/ExportYourData.tsx
+++ app/src/pwa/ExportYourData.tsx
@@ -1,5 +1,30 @@
 import { Button } from "@mui/material";
+import { ServerApi } from "../serverApi/ServerApi";
 
 export const ExportYourData: React.FC = () => {
-  return <Button variant="outlined">TODO: Export</Button>;
+  return (
+    <Button variant="outlined" onClick={exportData}>
+      Export
+    </Button>
+  );
+
+  async function exportData() {
+    const exportedData = await ServerApi.exportData();
+
+    const url = URL.createObjectURL(
+      new Blob([JSON.stringify(exportedData)], {
+        type: "application/json",
+      }),
+    );
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "data.json";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  }
 };
