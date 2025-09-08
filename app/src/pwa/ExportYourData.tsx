b1b05335604867f7f95a9fc41fb28d38046c50b2
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-09-08 14:38:34 +0200

    Cleanup

--- app/src/pwa/ExportYourData.tsx
+++ app/src/pwa/ExportYourData.tsx
@@ -1,14 +1,19 @@
 import { Button } from "@mui/material";
 import { ServerApi } from "../serverApi/ServerApi";
+import React, { useState } from "react";
 
 export const ExportYourData: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <Button variant="outlined" onClick={exportData}>
+    <Button variant="outlined" onClick={exportData} disabled={isLoading}>
       Export
     </Button>
   );
 
   async function exportData() {
+    setIsLoading(true);
+
     const exportedData = await ServerApi.exportData();
 
     const url = URL.createObjectURL(
@@ -19,12 +24,14 @@ export const ExportYourData: React.FC = () => {
 
     const link = document.createElement("a");
     link.href = url;
-    link.download = "data.json";
+    link.download = `engraved-user-data-${new Date().toISOString()}.json`;
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 
     URL.revokeObjectURL(url);
+
+    setIsLoading(false);
   }
 };
