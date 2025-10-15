501f04501373a6dde6b9d639fa84a1eb8e01ce98
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-10-15 18:18:10 +0200

    NEW: trying out emoji

--- app/src/components/details/scraps/markdown/LazyMarkdown.tsx
+++ app/src/components/details/scraps/markdown/LazyMarkdown.tsx
@@ -5,18 +5,6 @@ import { getMarkdownInstance } from "./getMarkdownInstance";
 
 const md = getMarkdownInstance();
 
-// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
-const defaultRender =
-  md.renderer.rules.link_open ||
-  function (tokens, idx, options, _, self) {
-    return self.renderToken(tokens, idx, options);
-  };
-
-md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-  tokens[idx].attrSet("target", "_blank");
-  return defaultRender(tokens, idx, options, env, self);
-};
-
 const LazyMarkdown: React.FC<IMarkdownProps> = ({
   value,
   onClick,
--- app/src/components/details/scraps/markdown/getMarkdownInstance.ts
+++ app/src/components/details/scraps/markdown/getMarkdownInstance.ts
@@ -10,6 +10,18 @@ md.renderer.rules.emoji = function (token, idx) {
   return `<span class="emoji">${token[idx].content}</span>`;
 };
 
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, _, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  tokens[idx].attrSet("target", "_blank");
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 export function getMarkdownInstance() {
   return md;
 }
