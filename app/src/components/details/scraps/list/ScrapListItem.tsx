40b9833d6c73ffea77a1b881b1a64f9ceb94528f
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-10-27 23:11:53 +0100

    Revert to AutogrowTextField for ScrapListItem

--- app/src/components/details/scraps/list/ScrapListItem.tsx
+++ app/src/components/details/scraps/list/ScrapListItem.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IScrapListItem } from "./IScrapListItem";
-import { Checkbox, styled, SxProps } from "@mui/material";
+import { Checkbox, styled } from "@mui/material";
 import DragIndicator from "@mui/icons-material/DragIndicator";
 import RemoveCircleOutline from "@mui/icons-material/RemoveCircleOutline";
+import { AutogrowTextField } from "../../../common/AutogrowTextField";
+import { SxProps } from "@mui/system";
 import { Markdown } from "../markdown/Markdown";
 import { ActionIconButtonGroup } from "../../../common/actions/ActionIconButtonGroup";
 import { ListItemCollection } from "./ListItemCollection";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { TextEditorNew } from "../../../common/TextEditorNew";
 
 export const ScrapListItem: React.FC<{
   listItemsCollection: ListItemCollection;
@@ -18,9 +19,15 @@ export const ScrapListItem: React.FC<{
   onChange: (listItem: IScrapListItem) => void;
 }> = ({ listItemsCollection, index, isEditMode, listItem, onChange }) => {
   const [label, setLabel] = useState(listItem.label);
+  const ref: React.RefObject<HTMLInputElement> = useRef(null);
 
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: listItemsCollection.getReactKey(index) });
+
+  useEffect(
+    () => listItemsCollection.setGiveFocus(index, () => ref.current.focus()),
+    [listItemsCollection, index],
+  );
 
   return (
     <ListItem
@@ -52,15 +59,13 @@ export const ScrapListItem: React.FC<{
       />
       {isEditMode ? (
         <>
-          <TextEditorNew
-            css={getCss() as React.CSSProperties}
-            initialValue={label}
-            setValue={setLabel}
+          <AutogrowTextField
+            forwardInputRef={ref}
+            isContent={true}
+            value={label}
+            onChange={(event) => setLabel(event.target.value)}
             onKeyUp={keyUp}
             onKeyDown={keyDown}
-            setGiveFocus={(giveFocus: () => void) => {
-              listItemsCollection.setGiveFocus(index, giveFocus);
-            }}
             onBlur={() =>
               onChange({
                 label,
@@ -68,6 +73,8 @@ export const ScrapListItem: React.FC<{
                 depth: listItem.depth,
               })
             }
+            sx={{ ...getSx("textbox"), pr: 1, pt: "5px !important" }}
+            autoFocus={!listItem.label}
           />
           <ActionIconButtonGroup
             backgroundColor={"none"}
@@ -82,24 +89,33 @@ export const ScrapListItem: React.FC<{
           />
         </>
       ) : (
-        <ReadonlyContainer sx={{ ...getCss(), pt: "7px" }}>
+        <ReadonlyContainer sx={{ ...getSx("plain"), pt: "7px" }}>
           <Markdown value={label} useBasic={true}></Markdown>
         </ReadonlyContainer>
       )}
     </ListItem>
   );
 
-  function getCss(): React.CSSProperties | SxProps {
-    const css: React.CSSProperties | SxProps = { flexGrow: 1 };
+  function getSx(elementType: "plain" | "textbox") {
+    const sx: SxProps & { textarea?: SxProps } = { flexGrow: 1 };
 
-    if (listItem.isCompleted) {
-      css.textDecoration = "line-through";
+    if (!listItem.isCompleted) {
+      return sx;
     }
 
-    return css;
+    switch (elementType) {
+      case "textbox":
+        sx.textarea = { textDecoration: "line-through" };
+        break;
+      case "plain":
+        sx.textDecoration = "line-through";
+        break;
+    }
+
+    return sx;
   }
 
-  function keyDown(e: KeyboardEvent) {
+  function keyDown(e: React.KeyboardEvent<HTMLDivElement>) {
     switch (e.key) {
       case "ArrowUp": {
         if (e.altKey && e.ctrlKey) {
@@ -109,7 +125,6 @@ export const ScrapListItem: React.FC<{
         }
         break;
       }
-
       case "ArrowDown": {
         if (e.altKey && e.ctrlKey) {
           listItemsCollection.moveItemDown(index);
@@ -118,28 +133,25 @@ export const ScrapListItem: React.FC<{
         }
         break;
       }
-
       case "ArrowRight": {
         if (e.altKey && e.ctrlKey) {
           listItemsCollection.moveItemRight(index);
         }
         break;
       }
-
       case "ArrowLeft": {
         if (e.altKey && e.ctrlKey) {
           listItemsCollection.moveItemLeft(index);
         }
         break;
       }
-
       case "Enter": {
         e.preventDefault();
         break;
       }
 
       case "Backspace": {
-        const target = e.target as HTMLDivElement;
+        const target = e.target as HTMLTextAreaElement;
 
         if (
           target.innerText === undefined ||
@@ -147,17 +159,16 @@ export const ScrapListItem: React.FC<{
           target.innerText?.trim() === ""
         ) {
           listItemsCollection.removeItem(index);
-          listItemsCollection.moveFocusUp(index, "end");
+          // listItemsCollection.moveFocusUp(index, "end");
           return;
         }
 
-        // if (target.selectionStart !== target.selectionEnd) {
-        //   return;
-        // }
+        if (target.selectionStart !== target.selectionEnd) {
+          return;
+        }
 
         if (e.altKey && e.ctrlKey) {
           listItemsCollection.removeItem(index);
-          listItemsCollection.moveFocusUp(index, "end");
         }
 
         break;
@@ -165,15 +176,13 @@ export const ScrapListItem: React.FC<{
     }
   }
 
-  function keyUp(e: KeyboardEvent) {
+  function keyUp(e: React.KeyboardEvent<HTMLDivElement>) {
     switch (e.key) {
       case "Enter": {
         listItemsCollection.addItem(index);
-        //listItemsCollection.moveFocusDown(index, "end");
         e.preventDefault();
         break;
       }
-
       case " ": {
         if (e.ctrlKey) {
           onChange({
@@ -184,7 +193,6 @@ export const ScrapListItem: React.FC<{
         }
         break;
       }
-
       default: {
         onChange({
           label,
@@ -196,20 +204,16 @@ export const ScrapListItem: React.FC<{
     }
   }
 };
-
 const StyledCheckbox = styled(Checkbox)`
   padding: 5px 5px 5px 0;
-
   &.MuiCheckbox-colorPrimary {
     color: ${(p) => p.theme.palette.primary.main} !important;
   }
 `;
-
 const ListItem = styled("li")`
   display: flex;
   align-items: start;
 `;
-
 const ReadonlyContainer = styled("div")`
   word-break: break-word;
 `;
