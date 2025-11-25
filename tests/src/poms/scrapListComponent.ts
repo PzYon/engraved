9f518f3c70d78c81439406c41c77b8a4b7153e4f
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-11-25 09:50:26 +0100

    Focus item in tests

--- tests/src/poms/scrapListComponent.ts
+++ tests/src/poms/scrapListComponent.ts
@@ -45,7 +45,7 @@ export class ScrapListComponent {
   }
 
   async selectItem(index: number) {
-    await this.getListItem(index, 0).click();
+    await this.getListItem(index, 0).getByRole("textbox").click();
     await expect(this.getListItem(index, 0)).toBeFocused();
   }
 
