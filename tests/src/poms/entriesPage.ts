ea0abef55564380f7dfe0501f0a6736b9882c524
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-12-08 21:24:52 +0100

    Fix another test (maybe)

--- tests/src/poms/entriesPage.ts
+++ tests/src/poms/entriesPage.ts
@@ -1,11 +1,17 @@
 import { expect } from "@playwright/test";
 import { OverviewPage } from "./overviewPage";
+import { isAndroidTest } from "../utils/mobile";
 
 export class EntriesPage extends OverviewPage {
   async expectItem(index: number, title: string, content: string) {
     await expect(
       this.page.getByTestId("entries-list-item-" + index).getByText(title),
     ).toBeVisible();
+
+    if (isAndroidTest()) {
+      await this.page.getByTestId("entries-list-item-" + index).click();
+    }
+
     await expect(
       this.page.getByTestId("entries-list-item-" + index).getByText(content),
     ).toBeVisible();
