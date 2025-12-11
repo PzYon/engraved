0bc0755bc03799679346656f37e744ccd498e3a8
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-12-11 21:21:56 +0100

    index.html

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
       command: isCi ? "npm run e2e:start-api:ci" : "npm run e2e:start-api",
       url: apiBaseUrl,
       reuseExistingServer: !isCi,
-      stdout: "pipe",
+      stdout: isCi ? undefined : "pipe",
       stderr: "pipe",
       // for some reason, this api server takes a long
       // time to start on CI
