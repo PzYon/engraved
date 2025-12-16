4840e797e9a7574059c42dea86d854f2dcbf831f
Markus Doggweiler <markus.doggweiler@gmail.com>
2025-12-16 21:34:48 +0100

    Working on EntryDocumentMapper

--- api/Engraved.Persistence.Mongo/Source/DocumentTypes/Entries/EntryDocumentMapper.cs
+++ api/Engraved.Persistence.Mongo/Source/DocumentTypes/Entries/EntryDocumentMapper.cs
@@ -48,9 +48,98 @@ public static class EntryDocumentMapper
     Mapper = configuration.CreateMapper();
   }
 
-  public static EntryDocument ToDocument(IEntry journal)
+  public static EntryDocument ToDocument(IEntry entry)
   {
-    return Mapper.Map<EntryDocument>(journal)!;
+    return entry switch
+    {
+      CounterEntry ce => MapCounterEntry(ce),
+      GaugeEntry ge => MapGaugeEntry(ge),
+      TimerEntry te => MapTimerEntry(te),
+      ScrapsEntry se => MapScrapsEntry(se),
+      _ => throw new ArgumentOutOfRangeException(nameof(entry), entry, null)
+    };
+  }
+
+  private static CounterEntryDocument MapCounterEntry(CounterEntry entry)
+  {
+    return new CounterEntryDocument
+    {
+      UserId = entry.UserId,
+      ParentId = entry.ParentId,
+      Notes = entry.Notes,
+      DateTime = entry.DateTime,
+      EditedOn = entry.EditedOn,
+      JournalAttributeValues = entry.JournalAttributeValues,
+      Schedules = MapSchedules(entry.Schedules)
+    };
+  }
+
+  private static GaugeEntryDocument MapGaugeEntry(GaugeEntry entry)
+  {
+    return new GaugeEntryDocument
+    {
+      UserId = entry.UserId,
+      ParentId = entry.ParentId,
+      Notes = entry.Notes,
+      DateTime = entry.DateTime,
+      EditedOn = entry.EditedOn,
+      JournalAttributeValues = entry.JournalAttributeValues,
+      Schedules = MapSchedules(entry.Schedules),
+      Value = entry.Value
+    };
+  }
+
+  private static TimerEntryDocument MapTimerEntry(TimerEntry entry)
+  {
+    return new TimerEntryDocument
+    {
+      UserId = entry.UserId,
+      ParentId = entry.ParentId,
+      Notes = entry.Notes,
+      DateTime = entry.DateTime,
+      EditedOn = entry.EditedOn,
+      JournalAttributeValues = entry.JournalAttributeValues,
+      Schedules = MapSchedules(entry.Schedules),
+      StartDate = entry.StartDate ?? default,
+      EndDate = entry.EndDate
+    };
+  }
+
+  private static ScrapsEntryDocument MapScrapsEntry(ScrapsEntry entry)
+  {
+    return new ScrapsEntryDocument
+    {
+      UserId = entry.UserId,
+      ParentId = entry.ParentId,
+      Notes = entry.Notes,
+      DateTime = entry.DateTime,
+      EditedOn = entry.EditedOn,
+      JournalAttributeValues = entry.JournalAttributeValues,
+      Schedules = MapSchedules(entry.Schedules),
+      Title = entry.Title,
+      ScrapType = entry.ScrapType
+    };
+  }
+
+  private static Dictionary<string, ScheduleSubDocument> MapSchedules(Dictionary<string, Schedule> schedules)
+  {
+    var result = new Dictionary<string, ScheduleSubDocument>();
+    
+    foreach (var (key, schedule) in schedules)
+    {
+      result[key] = new ScheduleSubDocument
+      {
+        NextOccurrence = schedule.NextOccurrence,
+        Recurrence = schedule.Recurrence != null 
+          ? new RecurrenceSubDocument { DateString = schedule.Recurrence.DateString }
+          : null,
+        DidNotify = schedule.DidNotify,
+        NotificationId = schedule.NotificationId,
+        OnClickUrl = schedule.OnClickUrl
+      };
+    }
+    
+    return result;
   }
 
   public static TEntry FromDocument<TEntry>(EntryDocument? document)
--- api/Engraved.Persistence.Mongo/Source/DocumentTypes/Journals/JournalDocumentMapper.cs
+++ api/Engraved.Persistence.Mongo/Source/DocumentTypes/Journals/JournalDocumentMapper.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Engraved.Core.Domain.Journals;
+﻿using Engraved.Core.Domain.Journals;
 using Engraved.Core.Domain.Schedules;
 using MongoDB.Bson;
 
@@ -7,49 +6,6 @@ namespace Engraved.Persistence.Mongo.DocumentTypes.Journals;
 
 public static class JournalDocumentMapper
 {
-  private static readonly IMapper Mapper;
-
-  static JournalDocumentMapper()
-  {
-    var configuration = new MapperConfiguration(cfg =>
-      {
-        cfg.CreateMap<IJournal, JournalDocument>()
-          .Include<CounterJournal, CounterJournalDocument>()
-          .Include<GaugeJournal, GaugeJournalDocument>()
-          .Include<TimerJournal, TimerJournalDocument>()
-          .Include<ScrapsJournal, ScrapsJournalDocument>();
-
-        cfg.CreateMap<CounterJournal, CounterJournalDocument>();
-        cfg.CreateMap<GaugeJournal, GaugeJournalDocument>();
-        cfg.CreateMap<TimerJournal, TimerJournalDocument>();
-        cfg.CreateMap<ScrapsJournal, ScrapsJournalDocument>();
-
-        cfg.CreateMap<Recurrence, RecurrenceSubDocument>();
-        cfg.CreateMap<Schedule, ScheduleSubDocument>();
-        cfg.CreateMap<ThresholdDefinition, ThresholdDefinitionDocument>();
-
-        cfg.CreateMap<JournalDocument, IJournal>()
-          .Include<CounterJournalDocument, CounterJournal>()
-          .Include<GaugeJournalDocument, GaugeJournal>()
-          .Include<TimerJournalDocument, TimerJournal>()
-          .Include<ScrapsJournalDocument, ScrapsJournal>();
-
-        cfg.CreateMap<CounterJournalDocument, CounterJournal>();
-        cfg.CreateMap<GaugeJournalDocument, GaugeJournal>();
-        cfg.CreateMap<TimerJournalDocument, TimerJournal>();
-        cfg.CreateMap<ScrapsJournalDocument, ScrapsJournal>();
-
-        cfg.CreateMap<RecurrenceSubDocument, Recurrence>();
-        cfg.CreateMap<ScheduleSubDocument, Schedule>();
-        cfg.CreateMap<ThresholdDefinitionDocument, ThresholdDefinition>();
-      }
-    );
-
-    // configuration.AssertConfigurationIsValid();
-
-    Mapper = configuration.CreateMapper();
-  }
-
   public static JournalDocument ToDocument(IJournal journal)
   {
     return journal switch
