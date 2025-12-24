354f72f499badd0433a29cf4e4e4a9404e3922b3
Markus Doggweiler <PzYon@users.noreply.github.com>
2025-12-25 00:43:21 +0100

    Cleanup some project files (#2671)

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    coverage: {
-      reporter: ["text", "json", "html"],
-    },
     globals: true,
-    // setupFiles: ['./src/setupTests.ts'], // Uncomment if you have a setup file
   },
 });
