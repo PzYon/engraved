44b50091e30f0fefc5772ef95cba8eb8bbad5369
Markus Doggweiler <markus.doggweiler@gmail.com>
2026-04-04 11:57:23 +0200

    csharpier

--- api/Engraved.Core/Source/Domain/Entries/ScrapsEntry.cs
+++ api/Engraved.Core/Source/Domain/Entries/ScrapsEntry.cs
@@ -4,8 +4,9 @@ public class ScrapsEntry : BaseEntry
 {
   public string Title { get; set; } = null!;
 
-  public ScrapType ScrapType { get; set; } = ScrapType.Markdown;
+      public ScrapType ScrapType { get; set; } = ScrapType.Markdown;
 
+      
   public override double GetValue()
   {
     throw new NotImplementedException();
