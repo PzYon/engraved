4c2c7b96ae185b68d195f6fa8153c743e90d9597
Markus Doggweiler <PzYon@users.noreply.github.com>
2026-04-17 22:02:22 +0200

    Fix Favorite icon (#2748)

--- app/src/components/layout/AppHeader.tsx
+++ app/src/components/layout/AppHeader.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import { Box, styled, Typography, useTheme } from "@mui/material";
 import { useAppContext } from "../../AppContext";
 import { ActionIconButtonGroup } from "../common/actions/ActionIconButtonGroup";
@@ -40,11 +40,11 @@ export const AppHeader: React.FC = () => {
 
   const { isCompact, setIsCompact } = useDisplayModeContext();
 
-  const loc = useLocation();
-
-  useEffect(() => {
-    console.log(`Router: Navigating to ${loc.pathname}${loc.search}`);
-  }, [loc.key, loc.pathname, loc.search]);
+  // const loc = useLocation();
+  //
+  // useEffect(() => {
+  //   console.log(`Router: Navigating to ${loc.pathname}${loc.search}`);
+  // }, [loc.key, loc.pathname, loc.search]);
 
   const deviceWidth = useDeviceWidth();
   const isSmall = deviceWidth === DeviceWidth.Small;
