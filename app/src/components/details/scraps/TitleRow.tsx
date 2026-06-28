d7a5158c946ae678f23f1c6c8b1fd053754d4928
Markus Doggweiler <markus.doggweiler@gmail.com>
2026-06-29 01:09:23 +0200

    Only show border-bottom when not compatc

--- app/src/components/details/scraps/TitleRow.tsx
+++ app/src/components/details/scraps/TitleRow.tsx
@@ -1,6 +1,16 @@
 import { styled } from "@mui/material";
+import { useDisplayModeContext } from "../../overview/overviewList/DisplayModeContext";
+import React from "react";
 
-export const TitleRow = styled("div")`
+export const TitleRow: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { isCompact } = useDisplayModeContext();
+
+  return isCompact ? <>{children}</> : <FullRow>{children}</FullRow>;
+};
+
+const FullRow = styled("div")`
   border-bottom: 1px solid ${(p) => p.theme.palette.background.default};
   padding-bottom: 6px;
   margin-bottom: 4px;
